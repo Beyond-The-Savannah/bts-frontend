bd73833ea50653bb7428caae5c1da40df95a932f
Michael Gitonga <gitoshmbae@gmail.com>
2025-01-06 17:20:47 +0300

    Adss tailwind prose functionality by add formatting

--- src/app/view-job/[jobsId]/page.tsx
+++ src/app/view-job/[jobsId]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { SingleRemoteJob } from "@/types/remoteJobsListing";
+import {  SingleRemoteJob } from "@/types/remoteJobsListing";
 import { useQuery } from "@tanstack/react-query";
 import { use } from "react";
 
@@ -9,6 +9,7 @@ export default function SinglJobListingPage({
   params: Promise<{ jobsId: string }>;
 }) {
   const { jobsId } = use(params);
+
   async function fetchSingleRemoteList(): Promise<SingleRemoteJob[]> {
     const result = fetch(
       `https://efmsapi.azurewebsites.net/api/Jobs/getAllJobsSections?jobId=${jobsId}`
@@ -16,6 +17,15 @@ export default function SinglJobListingPage({
     return result;
   }
 
+//   async function fetchSingleRemoteListingDetails2(): Promise<ListingRemoteJobs> {
+//     const result =
+//       fetch(`https://efmsapi.azurewebsites.net/api/Jobs/getAllJobsByCompany?jobCategoryId=2
+// `).then((res) => res.json());
+//     return result;
+//   }
+
+//   const resultDetails2=fetchSingleRemoteListingDetails2()
+//   const filteredJob=resultDetails2.find(())
   const {
     data: singleJob,
     isLoading,
@@ -35,17 +45,24 @@ export default function SinglJobListingPage({
     <>
       <section className="container mx-auto  min-h-screen">
         <span>{jobsId}</span>
-        {singleJob && (
-          <>
-            {singleJob.map((listing) => (
-              <div key={listing.id}>
-                <p className="text-xl font-semibold">{listing.sectionName}</p>
-                <h3>{listing.sectionDescription}</h3>
-              </div>
-            ))}
-          </>
-        )}
-        <p>We cannot find that particular job listing</p>
+        <div className="space-y-12 grid grid-cols-1 md:grid-cols-2 items-start justify-center gap-4">
+          {singleJob && (
+            <>
+              {singleJob.map((listing) => (
+                <article
+                  key={listing.id}
+                  className=" border rounded-lg py-4  px-8"
+                >
+                  <p className="text-xl font-semibold">{listing.sectionName}</p>
+                  <h3>
+                    {`<${listing.sectionDescription}`}
+                  </h3>
+                </article>
+              ))}
+            </>
+          )}
+        </div>
+        {!singleJob && <p>We cannot find that particular job listing</p>}
       </section>
     </>
   );
