6eb8da032ab131e828fd00545e618c8d852294be
Michael Gitonga <gitoshmbae@gmail.com>
2025-01-07 19:07:32 +0300

    adds a new Hero image for the home page

--- src/components/homePage/HeroSection.tsx
+++ src/components/homePage/HeroSection.tsx
@@ -5,8 +5,9 @@ import Image from "next/image";
 export default function HeroSection() {
   return (
     <>
-      <section className="container mx-auto px-4 min-h-screen grid grid-cols-1 md:grid-cols-2 pt-40 md:pt-0 items-center gap-4">
-        <div className=" space-y-6   capitalize">
+      {/* <section className="container mx-auto px-4 min-h-screen grid grid-cols-1 md:grid-cols-2 pt-40 md:pt-0 items-center gap-4"> */}
+      <section className="container mx-auto px-4 min-h-[90vh] grid grid-cols-1 md:grid-cols-2  md:pt-0  gap-4">
+        <div className=" space-y-6 mt-40 md:mt-64 capitalize">
           <h1 className=" text-3xl lg:text-5xl text-balance font-bold">
             Empowering your career journey through seamless connections.
           </h1>
@@ -20,18 +21,16 @@ export default function HeroSection() {
             </Button>
           </Link>
         </div>
-        <div className="relative -mt-12 lg:-mt-0">
-          <div className="size-48 md:size-96 rounded-md  bg-amber-600 absolute top-80 left-40 -z-10"></div>
+        <div className="">
+          {/* <div className="size-48 md:size-96 rounded-md  bg-amber-600 absolute top-80 left-40 -z-10"></div>
           <div className="size-32 md:size-64 rounded-md  bg-amber-400 absolute top-40 md:top-64 -z-10"></div>
-          <div className="size-32 md:size-64 rounded-md  bg-amber-400 absolute bottom-12 md:bottom-24 -z-10"></div>
+          <div className="size-32 md:size-64 rounded-md  bg-amber-400 absolute bottom-12 md:bottom-24 -z-10"></div> */}
           <Image
-            src={
-              "https://i.postimg.cc/V6JsZmXH/DSC-0811-copyk-removebg-preview.png"
-            }
+            src="/images/Beyond The Savannah Home Hero .png"
             width={800}
             height={800}
-            alt="LORRAINE founder of beyond the savannah"
-            className="object-conver h-full w-full  lg:-mt-48"
+            alt="Lorraine founder of beyond the savannah"
+            className="object-cover h-full w-full  "
           />
         </div>
       </section>
