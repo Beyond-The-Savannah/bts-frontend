6c6c8f78caedceb64cc3a170f21a75fdc7b6b254
Michael Gitonga <gitoshmbae@gmail.com>
2025-01-10 10:20:24 +0300

    adds the missing components for the mobile navigation section in src/components/NavigationSection.tsx

--- src/components/NavigationSection.tsx
+++ src/components/NavigationSection.tsx
@@ -11,6 +11,16 @@ import {
 } from "./ui/navigation-menu";
 import Image from "next/image";
 import { Link } from "next-view-transitions";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { MenuSquareIcon } from "lucide-react";
 
 export default function NavigationSection() {
   return (
@@ -29,11 +39,14 @@ export default function NavigationSection() {
                 />
               </Link>
             </NavigationMenuItem>
-            <div className="flex  items-end md:items-center gap-3 lg:gap-8">
+            <div className="hidden md:flex  items-end md:items-center gap-3 lg:gap-8">
               {simpleNavigationMenuItems.map((menuItem) => (
                 <NavigationMenuItem key={menuItem.id} className="">
-                  <Link href={`${menuItem.link}`} className="font-semibold flex hover:scale-110 transition ease-in">
-                  {menuItem.title}
+                  <Link
+                    href={`${menuItem.link}`}
+                    className="font-semibold flex hover:scale-110 transition ease-in"
+                  >
+                    {menuItem.title}
                   </Link>
                 </NavigationMenuItem>
               ))}
@@ -61,6 +74,63 @@ export default function NavigationSection() {
                   </NavigationMenuContent>
                 </NavigationMenuItem>
               ))}
+            </div>
+            {/* mobile navigation  */}
+            <div className="block md:hidden">
+              <Sheet>
+                <SheetTrigger>
+                  <MenuSquareIcon className="" size={32} />
+                </SheetTrigger>
+                <SheetContent className="overflow-auto">
+                  <SheetHeader>
+                    <SheetTitle></SheetTitle>
+                    <SheetDescription></SheetDescription>
+                  </SheetHeader>
+                  <div className="">
+                    <NavigationMenu>
+                      <NavigationMenuList className="flex flex-col items-start gap-8">
+                        {simpleNavigationMenuItems.map((menuItem) => (
+                          <NavigationMenuItem key={menuItem.id} className=" list-none">
+                            <SheetClose asChild>
+                              <Link
+                                href={`${menuItem.link}`}
+                                className="font-semibold hover:scale-110 transition ease-in"
+                              >
+                                {menuItem.title}
+                              </Link>
+                            </SheetClose>
+                          </NavigationMenuItem>
+                        ))}
+
+                        {NonSimpleNavigationMenuItems.map((item) => (
+                          <NavigationMenuItem key={item.id} className="list-none">
+                            <NavigationMenuTrigger className=" px-0 font-semibold text-base relative bg-transparent hover:bg-transparent hover:scale-110 transition ease-in">
+                              {item.title}
+                            </NavigationMenuTrigger>
+                            <NavigationMenuContent className="">
+                              <ul className="flex flex-col px-4 py-2 space-y-4 w-64">
+                                {item.subLinks.map((sublink) => (
+                                  <li key={sublink.title}>
+                                    <SheetClose asChild className="">
+                                      <Link
+                                        href={sublink.link}
+                                        className="flex hover:scale-105 transition ease-in"
+                                      >
+                                        {sublink.title}
+                                      </Link>
+
+                                    </SheetClose>
+                                  </li>
+                                ))}
+                              </ul>
+                            </NavigationMenuContent>
+                          </NavigationMenuItem>
+                        ))}
+                      </NavigationMenuList>
+                    </NavigationMenu>
+                  </div>
+                </SheetContent>
+              </Sheet>
             </div>
           </NavigationMenuList>
         </NavigationMenu>
