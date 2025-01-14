2ca20af6181423cd2df2b0541b39e87b3a871981
Michael Gitonga <gitoshmbae@gmail.com>
2025-01-14 14:52:39 +0300

    adds meta data for the about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,28 @@
 import CommitmentSection from "@/components/aboutPage/CommitmentSection";
 import FounderSection from "@/components/aboutPage/FounderSection";
 import HeroSection from "@/components/aboutPage/HeroSection";
+import { Metadata } from "next";
+import { getCldImageUrl } from "next-cloudinary";
 // import WhySection from "@/components/aboutPage/WhySection";
 
+
+const url= getCldImageUrl({
+  src:"about_-_beyond_the_savannah_zxeekw"
+})
+
+export const metadata:Metadata={
+  openGraph:{
+    images:[
+      {
+        width:1200,
+        height:627,
+        url
+      }
+    ]
+  },
+  title:"About Beyond The Savannah",
+  description:"Discover your potential at Beyond the Savannah, where we connect talented professionals with rewarding remote job opportunities. Embrace the future of work and achieve a fulfilling career from anywhere in the world."
+}
 
 export default function AboutPage() {
   return (
