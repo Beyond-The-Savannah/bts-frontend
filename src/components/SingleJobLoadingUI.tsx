118c460bf596e82fda27128683dd1a306d620abd
Michael Gitonga <gitoshmbae@gmail.com>
2025-01-14 14:21:41 +0300

    adjusts the color and makes the loading UI to be repsonsive in  src/components/SingleJobLoadingUI.tsx

--- src/components/SingleJobLoadingUI.tsx
+++ src/components/SingleJobLoadingUI.tsx
@@ -3,21 +3,15 @@ export default function SingleJobLoadingUI() {
     <>
       <section className="container mx-auto min-h-screen">
         <div className="flex flex-wrap items-start justify-center py-10 gap-8">
-          {/* {[1, 2].map((item) => ( */}
-          <div
-            // key={item}
-            // className="animate-pulse border rounded-xl w-5/12 space-y-4 bg-slate-100 px-8 py-4"
-            className="  w-5/12 space-y-4  px-8 py-4"
-          >
+          <div className="  w-full md:w-5/12 space-y-4  px-8 py-4">
             {[1, 2, 3, 4].map((item2) => (
               <div
                 key={item2}
-                className=" animate-pulse border rounded-xl w-full h-64 bg-slate-200 px-8 py-4"
+                className=" animate-pulse border rounded-xl w-full h-64 bg-bts-BrownFive px-8 py-4"
               ></div>
             ))}
           </div>
-          <div className="animate-pulse border rounded-xl w-[30vw]  h-[70vh] bg-slate-100 px-8 py-4"></div>
-          {/* ))} */}
+          <div className="animate-pulse border rounded-xl w-full md:w-[40vw]  h-[70vh] bg-bts-BrownOne px-8 py-4"></div>
         </div>
       </section>
     </>
