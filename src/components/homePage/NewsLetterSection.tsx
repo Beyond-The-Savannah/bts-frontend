2168136d54a795ee832df669d875eb0c11f8bd88
Michael Gitonga <gitoshmbae@gmail.com>
2025-01-22 11:36:58 +0300

    changes the newsLetterSection copy to new copy and message that is not newsLetter related

--- src/components/homePage/NewsLetterSection.tsx
+++ src/components/homePage/NewsLetterSection.tsx
@@ -1,19 +1,46 @@
-import { Button } from "../ui/button";
+import DisplayImageFromNextCloudinary from "../DisplayImageFromNextCloudinary";
+// import { Button } from "../ui/button";
 
 export default function NewsLetterSection() {
   return (
     <>
-      <section className="container mx-auto px-4 mt-64 h-full lg:h-[50vh] flex flex-col justify-center">
+      <section className="container mx-auto px-4 mt-64 mb-20 h-full lg:min-h-[50vh] flex flex-col justify-center">
         <div className="">
-          <h2 className="text-xl">News letter</h2>
-          {/* <div className="border-2 rounded-md border-stone-500 w-36"></div> */}
+          {/* <h2 className="text-xl">News letter</h2> */}
+          <h2 className="text-xl">Global Workforce</h2>
+
           <div className="border-2 rounded-md border-bts-BrownThree w-36"></div>
           <p className="capitalize text-3xl font-bold text-bts-GreenOne mt-2">
-            Get curated, remote jobs listings
+            {/* Get curated, remote jobs listings */}
+            Work From Anywhere
           </p>
         </div>
-        <div className="max-w-3xl mx-auto my-8">
-          {/* <div className="text-center text-balance space-y-8 py-4 px-8 bg-amber-100 rounded-lg"> */}
+        <div className="flex flex-wrap md:flex-nowrap items-center justify-center gap-12 max-w-6xl  mx-auto px-4">
+          <div className="w-full xl:w-[50vw]">
+            <DisplayImageFromNextCloudinary
+              src="global_map_anodbs"
+              height={800}
+              width={800}
+              alt="word map"
+              classname="object-cover h-full w-full"
+            />
+          </div>
+          <div className="w-full xl:w-[50vw] space-y-8 text-balance">
+            <p className="text-2xl font-semibold">
+              A World Wide  WorkForce
+            </p>
+            <p>
+              Step into the future of employment, where the world is your office. Our extensive listings
+              of remote jobs allow you to work from anywhere. <br/><br/>Become part of the
+              global workforce, where boundaries dissolve, and possibilities
+              expand. Our platform offers a diverse array of remote job listings
+              that empower you to work from anywhere in the world.
+            </p>
+           
+          </div>
+        </div>
+        {/* <div className="max-w-3xl mx-auto my-8">
+          
           <div className="text-center text-balance space-y-8 py-4 px-8 bg-bts-BrownTwo rounded-lg">
             <article>
                 <p className="text-3xl font-semibold">
@@ -32,7 +59,7 @@ export default function NewsLetterSection() {
                 </Button>
             </form>
           </div>
-        </div>
+        </div> */}
       </section>
     </>
   );
