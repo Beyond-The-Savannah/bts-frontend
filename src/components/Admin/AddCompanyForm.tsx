104988ab84169e596b74f02e0961ec448b7ef7bf
Michael Gitonga <gitoshmbae@gmail.com>
2025-01-23 17:01:48 +0300

    Adds new form that adds company information, Adds a textarea from shadcnui

--- src/components/Admin/AddCompanyForm.tsx
+++ src/components/Admin/AddCompanyForm.tsx
@@ -1,131 +1,156 @@
-'use client'
+"use client";
 
-import { useForm } from "react-hook-form"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
-import { z } from "zod"
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { z } from "zod";
 import { CompanyFormSchema } from "@/formSchemas/jobListingSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components//ui/input";
 import { Button } from "@/components/ui/button";
+import { Textarea } from "../ui/textarea";
 
 export default function AddCompanyForm() {
+  const form = useForm<z.infer<typeof CompanyFormSchema>>({
+    resolver: zodResolver(CompanyFormSchema),
+    defaultValues: {
+      companyName: "",
+      companyHeadQuaters: "",
+      companyContactEmail: "",
+      companyContactPhone: "",
+      companyDescription: "",
+      location: "",
+      imageUrl: "",
+    },
+  });
 
-    const form=useForm<z.infer<typeof CompanyFormSchema>>({
-        resolver:zodResolver(CompanyFormSchema),
-        defaultValues:{
-            companyName:"",
-            companyHeadQuaters:"",
-            companyContactEmail:"",
-            companyContactPhone:"",
-            companyDescription:"",
-            location:"",
-            imageUrl:"",
-        }
-    })
+  function onSubmit(data: z.infer<typeof CompanyFormSchema>) {
+    alert(JSON.stringify(data));
+  }
 
-    function onSubmit(data:z.infer<typeof CompanyFormSchema>){
-        alert(JSON.stringify(data))
-    }
-  
-    return (
+  return (
     <>
-    <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+    <div className="mb-20">
+        <h2 className="text-xl">Company Form</h2>
+            <div className="border-2 rounded-md border-bts-BrownThree w-36"></div>
+            <p className="capitalize text-3xl font-bold text-bts-GreenOne mt-2">
+                Add The Company Details
+            </p>
+
+    </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-12">
+          <div className="flex flex-wrap items-center justify-center gap-6">
             <FormField
-                control={form.control}
-                name="companyName"
-                render={({field})=>(
-                    <FormItem>
-                        <FormLabel>Company Name</FormLabel>
-                        <FormControl>
-                            <Input {...field}/>
-                        </FormControl>
-                        <FormMessage/>
-                    </FormItem>
-                )}
+              control={form.control}
+              name="companyName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Company Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} className="w-[90vw] md:w-[30vw] lg:w-[24vw]" required />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
             <FormField
-                control={form.control}
-                name="companyHeadQuaters"
-                render={({field})=>(
-                    <FormItem>
-                        <FormLabel>Company HeadQuaters</FormLabel>
-                        <FormControl>
-                            <Input {...field}/>
-                        </FormControl>
-                        <FormMessage/>
-                    </FormItem>
-                )}
-            />
-            <FormField
-                control={form.control}
-                name="companyContactEmail"
-                render={({field})=>(
-                    <FormItem>
-                        <FormLabel>Company Contact Email</FormLabel>
-                        <FormControl>
-                            <Input {...field}/>
-                        </FormControl>
-                        <FormMessage/>
-                    </FormItem>
-                )}
-            />
-            <FormField
-                control={form.control}
-                name="companyContactPhone"
-                render={({field})=>(
-                    <FormItem>
-                        <FormLabel>Company Contact Phone</FormLabel>
-                        <FormControl>
-                            <Input {...field}/>
-                        </FormControl>
-                        <FormMessage/>
-                    </FormItem>
-                )}
-            />
-            <FormField
-                control={form.control}
-                name="companyDescription"
-                render={({field})=>(
-                    <FormItem>
-                        <FormLabel>Company Description</FormLabel>
-                        <FormControl>
-                            <Input {...field}/>
-                        </FormControl>
-                        <FormMessage/>
-                    </FormItem>
-                )}
-            />
-            <FormField
-                control={form.control}
-                name="location"
-                render={({field})=>(
-                    <FormItem>
-                        <FormLabel>Location</FormLabel>
-                        <FormControl>
-                            <Input {...field}/>
-                        </FormControl>
-                        <FormMessage/>
-                    </FormItem>
-                )}
-            />
-            <FormField
-                control={form.control}
-                name="imageUrl"
-                render={({field})=>(
-                    <FormItem>
-                        <FormLabel>Image Url</FormLabel>
-                        <FormControl>
-                            <Input {...field}/>
-                        </FormControl>
-                        <FormMessage/>
-                    </FormItem>
-                )}
+              control={form.control}
+              name="companyHeadQuaters"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Company Headquaters</FormLabel>
+                  <FormControl>
+                    <Input {...field} className="w-[90vw] md:w-[30vw] lg:w-[24vw]" required />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
 
-            <Button  type="submit" className="bg-bts-BrownThree hover:bg-green-800">Add Company</Button>
+            <FormField
+              control={form.control}
+              name="companyContactEmail"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Company Contact Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} className="w-[90vw] md:w-[30vw] lg:w-[24vw]" required />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex flex-wrap items-center justify-center gap-6">
+            <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Location</FormLabel>
+                  <FormControl>
+                    <Input {...field} className="w-[90vw] md:w-[30vw] lg:w-[24vw]" required />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="companyContactPhone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Company Contact Phone</FormLabel>
+                  <FormControl>
+                    <Input {...field} className="w-[90vw] md:w-[30vw] lg:w-[24vw]" required type='tel' />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="imageUrl"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Image Url</FormLabel>
+                  <FormControl>
+                    <Input {...field} className="w-[90vw] md:w-[30vw] lg:w-[24vw]" required />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormField
+            control={form.control}
+            name="companyDescription"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company Description</FormLabel>
+                <FormControl>
+                    <Textarea {...field} className="resize" rows={5}/>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            type="submit"
+            className="bg-bts-BrownThree hover:bg-green-800"
+          >
+            Add Company
+          </Button>
         </form>
-    </Form>
+      </Form>
     </>
-  )
+  );
 }
--- src/formSchemas/jobListingSchema.ts
+++ src/formSchemas/jobListingSchema.ts
@@ -4,7 +4,7 @@ export const CompanyFormSchema= z.object({
     companyName:z.string().min(1,{message:"Comapny Name must be at least one character long"}),
     companyHeadQuaters:z.string().min(1,{message:"Comapny HeadQuaters must be at least one character long"}),
     companyContactEmail:z.string().email({message:"Email is Invalid"}),
-    companyContactPhone:z.string().min(1,{message:"Comapny Number must be at least one character long"}),
+    companyContactPhone:z.string().min(1,{message:"Invalid, needs to numbers "}),
     companyDescription:z.string().min(1,{message:"Comapny Description must be at least one character long"}),
     location:z.string().min(1,{message:"Comapny Location must be at least one character long"}),
     imageUrl:z.string().min(1,{message:"Comapny Image URL must be at least one character long"}),
