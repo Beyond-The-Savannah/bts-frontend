76ece520bc24cc792b9772817054cdb784464669
Michael Gitonga <gitoshmbae@gmail.com>
2025-02-06 17:27:53 +0300

    Adds a resend and react email packages, adds email template in src/components/Emails/, adds and end point that sends emails in src/app/api/send

---
+++ src/app/api/send/route.ts
@@ -0,0 +1,26 @@
+import EmailTemplate from "@/components/Emails/EmailTemplate";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function GET() {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: `Acme <onboarding@resend.dev>`,
+      to: [`gitoshmbae@gmail.com`, `gitonga1993@gmail.com`],
+      subject: `Beyond The Savannah Service Link`,
+      react: EmailTemplate(
+    //     {
+    //     email: "test@mail.com",
+    //   }
+    ),
+    });
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+    console.log(data);
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
