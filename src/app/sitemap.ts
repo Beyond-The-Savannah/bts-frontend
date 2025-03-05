feddb23e952a3a34c151477c29eb541e9dfee81e
Michael Gitonga <gitoshmbae@gmail.com>
2025-03-05 10:12:15 +0300

    Adds the missing properties of the sitemap for the servicesPages

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,25 +4,28 @@ import { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const servicesPages: MetadataRoute.Sitemap = servicesList.map((service) => ({
     url: `${process.env.NEXT_PUBLIC_BASE_URL}/service/${service.titleSlug}`,
+    lastModified: new Date(),
+    changeFrequency: "monthly",
+    priority: 1,
   }));
   return [
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority:1,
+      changeFrequency: "monthly",
+      priority: 1,
     },
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority:1,
+      changeFrequency: "monthly",
+      priority: 1,
     },
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/find-jobs`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority:1,
+      changeFrequency: "monthly",
+      priority: 1,
     },
     ...servicesPages,
   ];
