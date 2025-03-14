f69947f9f3c235f24c1769ed54df7b3dbacb4aa5
Michael Gitonga <gitoshmbae@gmail.com>
2025-03-14 13:19:43 +0300

    Comments out the console log messages in Packages, Removes commented out code, Adjust the Packages UI and copy

--- src/app/Customer/page.tsx
+++ src/app/Customer/page.tsx
@@ -1,22 +1,9 @@
 import PackageOptionSection from "@/components/Customer/PackageOptionSection";
 import SubscriptionDetails from "@/components/Customer/SubscriptionDetails";
 import { GetUserSubscriptionInformation } from "@/components/Customer/UserSubscriptionInformation";
-// import { SubscriptionProps } from "@/types/subscriptions";
-// import { currentUser } from "@clerk/nextjs/server";
 
 export default async function CustomerDefaultPage() {
   const userSubscriptionInformation = await GetUserSubscriptionInformation();
-  // const user = await currentUser();
-  // const response = await fetch(`http://localhost:3000/api/subscriptions`);
-  // const allSubscriptionData = await response.json();
-  // const userEmailAddress = user?.emailAddresses[0].emailAddress;
-
-  // const userSubscriptionInformation: SubscriptionProps =
-  //   allSubscriptionData.data.find(
-  //     (data: SubscriptionProps) => data.customer.email == userEmailAddress
-  //   );
-
-  // console.log("CUSTOMER_PAGE--", userSubscriptionInformation);
 
   return (
     <>
--- src/components/Customer/CareerSelection.tsx
+++ src/components/Customer/CareerSelection.tsx
@@ -6,10 +6,6 @@ export default function CareerSelection() {
       <div className="min-h-[30vh] space-y-6 px-4 py-8 bg-slate-200 rounded-lg w-full lg:max-w-lg">
         <div className="space-y-4">
           <p className="font-semibold text-xl">Career Selection</p>
-          {/* <p className="text-">
-            Please select a career category that best matches your carrer to
-            view the jobs listing
-          </p> */}
         </div>
         <div className="c">
           <CareerSelectionComponent />
