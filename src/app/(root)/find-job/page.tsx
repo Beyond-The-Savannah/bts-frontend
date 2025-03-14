4fa2d70ea5bb79e149601c683f84a009fdb9abad
Michael Gitonga <gitoshmbae@gmail.com>
2025-03-14 15:50:56 +0300

    Turns src/app/(root)/find-job/page.tsx to a client component

--- src/app/(root)/find-job/page.tsx
+++ src/app/(root)/find-job/page.tsx
@@ -1,10 +1,14 @@
+'use client'
 import { FindRemoteJobs } from "@/components/findJobsPage/FindRemoteJobs";
+import RemoteJobListingErrorUI from "@/components/RemoteJobListingErrorUI";
+import { Suspense } from "react";
 
 
 export default function page() {
   return (
-    
+    <Suspense fallback={<RemoteJobListingErrorUI/>}>
       <FindRemoteJobs />
+    </Suspense>
     
   );
 }
