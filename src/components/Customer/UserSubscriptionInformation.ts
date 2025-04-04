b2f00a3e12bbb9fdb1438750577364cea704faf2
Michael Gitonga <gitoshmbae@gmail.com>
2025-04-04 16:16:09 +0300

    Fixes typo in the status 'attention' check in src/app/Customer/page.tsx, Adds header to not use cached data in the fetch in src/components/Customer/UserSubscriptionInformation.ts

--- src/components/Customer/UserSubscriptionInformation.ts
+++ src/components/Customer/UserSubscriptionInformation.ts
@@ -2,29 +2,34 @@ import { AddSubscriberEmailToMailerLite } from "@/lib/mailerLite";
 import { SubscriptionProps } from "@/types/subscriptions";
 import { currentUser } from "@clerk/nextjs/server";
 
-const PUBLIC_BASE_URL=process.env.PUBLIC_BASE_URL
+const PUBLIC_BASE_URL = process.env.PUBLIC_BASE_URL;
 
- export async function GetUserSubscriptionInformation(){
+export async function GetUserSubscriptionInformation() {
   try {
     const user = await currentUser();
-    const response = await fetch(`${PUBLIC_BASE_URL}/api/subscriptions`);
-    if(!response.ok){
-      throw new Error(`HTTP error! status:${response.status}`)
+    const response = await fetch(`${PUBLIC_BASE_URL}/api/subscriptions`, {
+      headers: { "Cache-Control": "no-cache" },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status:${response.status}`);
     }
     const allSubscriptionData = await response.json();
     const userEmailAddress = user?.emailAddresses[0].emailAddress.toLowerCase();
-  
+
     const userSubscriptionInformation: SubscriptionProps =
       allSubscriptionData.data.find(
-        (data: SubscriptionProps) => data.customer.email.toLowerCase() == userEmailAddress
+        (data: SubscriptionProps) =>
+          data.customer.email.toLowerCase() == userEmailAddress
       );
     // console.log("SUBINFO", userSubscriptionInformation);
-    if(userSubscriptionInformation.status=='active' && userEmailAddress !=undefined){
-      await AddSubscriberEmailToMailerLite({email:userEmailAddress})
+    if (
+      userSubscriptionInformation.status == "active" &&
+      userEmailAddress != undefined
+    ) {
+      await AddSubscriberEmailToMailerLite({ email: userEmailAddress });
     }
-    return userSubscriptionInformation
-    
+    return userSubscriptionInformation;
   } catch (error) {
-  console.log("Error Getting userSubscriptionInformation",error)    
+    console.log("Error Getting userSubscriptionInformation", error);
   }
- }
+}
