04ef012c87e8b31a0e61473896e8075874bead4a
Michael Gitonga <gitoshmbae@gmail.com>
2025-05-03 09:45:14 +0300

    Fix a typo in the heading div in src/app/Customer/Kazina/page.tsx

--- src/app/Customer/Kazina/page.tsx
+++ src/app/Customer/Kazina/page.tsx
@@ -20,7 +20,7 @@ export default function page() {
           </div>
           <div className="border-2 rounded-md border-bts-BrownThree w-36"></div>
           <p className="capitalize text-xl md:text-3xl font-bold text-bts-GreenOne mt-2">
-            You remote work assisant
+            Your remote work assisant
           </p>
         </div>
         <KazinaChatUi />
