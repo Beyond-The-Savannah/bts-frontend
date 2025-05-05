d9e9397c27cea902abbd4fadaaf47f2d85464a13
Michael Gitonga <gitoshmbae@gmail.com>
2025-05-05 12:09:41 +0300

    Adjusts the UI width of the div showing a job to span as much with as possible across mobile devices

--- src/components/findJobsPage/FindJobs.tsx
+++ src/components/findJobsPage/FindJobs.tsx
@@ -77,7 +77,8 @@ export  function FindJobs() {
           {paginatedRemoteJobs?.map((job, index) => (
             <div
               key={index}
-              className=" border-bts-BrownTwo/50 border-2 rounded-xl w-full md:w-[22rem] lg:w-5/12 bg-bts-BrownTwo/50 hover:shadow-bts-BrownFour hover:shadow-md hover:bg-bts-BrownFive/50 duration-700 px-8 py-4"
+              // className=" border-bts-BrownTwo/50 border-2 rounded-xl w-full md:w-[38rem] lg:w-5/12  bg-bts-BrownTwo/50 hover:shadow-bts-BrownFour hover:shadow-md hover:bg-bts-BrownFive/50 duration-700 px-8 py-4"
+              className=" border-bts-BrownTwo/50 border-2 rounded-xl w-full md:w-12/12 lg:w-[31rem]   bg-bts-BrownTwo/50 hover:shadow-bts-BrownFour hover:shadow-md hover:bg-bts-BrownFive/50 duration-700 px-8 py-4"
             >
               <div className="flex items-center gap-2">
                 <Image
