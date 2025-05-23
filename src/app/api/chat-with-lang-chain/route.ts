4a8c9698b5c5169c720fa4fd17cbc797a55ad0af
Michael Gitonga <gitoshmbae@gmail.com>
2025-05-23 15:06:40 +0300

    Updates the model that savannah uses one that is snap shot based, Adds services consstraints to the services tips and pointers,  Includes the users profile photo in the chatUi, Adds a package to be used in react markdown to show links better,

--- src/app/api/chat-with-lang-chain/route.ts
+++ src/app/api/chat-with-lang-chain/route.ts
@@ -11,13 +11,14 @@ export const dynamic = "force-dynamic";
 const KazinaTemplate = `"You are Savannah, a remote work assistant at Beyond The Savannah.",
 "You are a specialist when it comes matters regarding remote work, CV writing, linkedin optimization, introductory video, and interview preparation.", 
 "You have access to the Beyond The Savannah website https://beyondthesavannah.co.ke from where you can recommend the services and package offerings to access the various job listings and other relevant information to users questions.",
-"",
-"When asked about job listings or a particular job, tell the user to have a active subscription by purchasing one of the packages. From the dashboard they can then filter the jobs listing based on the filters provided",
+"When asked about our services (ATS CV writing, LinkedIn Optimization, Introductory Video, and Interview Preparation), give them tips and pointers then go ahead and recommend them to purchase the service from the site for a more proffessional and tailored result ",
+"When asked about job listings or a particular job, tell the user to have a active subscription by purchasing one of the packages. From the dashboard they can then filter the jobs listing based on the filters of a job name or department of the job",
 " When asked questions not within the mentioned areas, please tell them you cannot help and should find a specialist for that topic."`;
 
 const llm = new ChatOpenAI({
   apiKey: process.env.OPEN_AI_KEY,
-  model: "gpt-4-turbo",
+  // model: "gpt-4.1-mini",
+  model: "gpt-4.1-mini-2025-04-14",
   temperature: 0,
 });
 const embeddings = new OpenAIEmbeddings();
