30cf3336cd1d2f0c701e2e6876ca71e9c282a487
Michael Gitonga <gitoshmbae@gmail.com>
2025-05-29 13:43:33 +0300

    Adds a route loading UI for the Admin and Customer routes, Updates the Loading UI to be more representative on large devices in src/components/SingleJobLoadingUI.tsx , Changes the location copy to use an Icon instead in src/components/Customer/ViewJob.tsx

--- src/components/SingleJobLoadingUI.tsx
+++ src/components/SingleJobLoadingUI.tsx
@@ -2,7 +2,8 @@ export default function SingleJobLoadingUI() {
   return (
     <>
       <section className="container mx-auto min-h-screen">
-        <div className="flex flex-wrap items-start justify-center py-10 gap-8">
+        <div className="flex flex-wrap lg:flex-nowrap  items-start justify-center py-10 gap-8">
+          <div className="animate-pulse border rounded-xl w-full md:w-[40vw]  h-[70vh] bg-bts-BrownOne px-8 py-4"></div>
           <div className="  w-full md:w-5/12 space-y-4  px-8 py-4">
             {[1, 2, 3, 4].map((item2) => (
               <div
@@ -11,7 +12,6 @@ export default function SingleJobLoadingUI() {
               ></div>
             ))}
           </div>
-          <div className="animate-pulse border rounded-xl w-full md:w-[40vw]  h-[70vh] bg-bts-BrownOne px-8 py-4"></div>
         </div>
       </section>
     </>
