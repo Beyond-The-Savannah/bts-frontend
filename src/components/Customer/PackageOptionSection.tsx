3683138596b4fc03c23941b76ef9a3e72a960af0
Michael Gitonga <gitoshmbae@gmail.com>
2025-06-03 12:56:36 +0300

    Adds a notification text about packages payment process in src/components/Customer/PackageOptionSection.tsx

--- src/components/Customer/PackageOptionSection.tsx
+++ src/components/Customer/PackageOptionSection.tsx
@@ -1,5 +1,6 @@
 import Packages from "@/components/Customer/Packages";
 import { currentUser } from "@clerk/nextjs/server";
+import { AlertCircle } from "lucide-react";
 
 export default async function PackageOptionSection() {
   const user = await currentUser();
@@ -15,12 +16,25 @@ export default async function PackageOptionSection() {
               Choose the package that fits your needs and budget.
             </p>
             <p className="max-w-2xl mx-auto">
-              Beyond The Savannah offers job listings with expert-led career preparation,
-              giving you the edge you need to stand out and secure your dream
-              role.
+              Beyond The Savannah offers job listings with expert-led career
+              preparation, giving you the edge you need to stand out and secure
+              your dream role.
             </p>
           </div>
           <Packages email={userEmail} />
+          <div className="max-w-xl mx-auto my-10">
+            <div className="c rounded-lg bg-amber-50 px-3 py-6 flex flex-col gap-4 items-start">
+              <AlertCircle className="size-4" />
+              <p className="text-xs">
+                Please note that all payments for the packages are
+                non-refundable once processed
+              </p>
+              <p className="text-xs">
+                To prevent subsequent charges, use the manage subscription
+                button in the dashboard
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </>
