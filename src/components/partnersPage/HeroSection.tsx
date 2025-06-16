0b724d5527562301542786974b245c2b78162533
Michael Gitonga <gitoshmbae@gmail.com>
2025-06-16 12:08:27 +0300

    Updates the cta button to dialog with a iframe to render the calendly page, Adjust the placement of the text in src/components/partnersPage/WhyPartnerships.tsx, Adjust the size of the sites logo for tablets in src/components/NavigationSection.tsx, Adds some padding top in src/components/partnersPage/HeroSection.tsx

--- src/components/partnersPage/HeroSection.tsx
+++ src/components/partnersPage/HeroSection.tsx
@@ -1,12 +1,13 @@
 import { Link } from "next-view-transitions";
 import DisplayImageFromNextCloudinary from "../DisplayImageFromNextCloudinary";
 import { Button } from "../ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
 
 export default function HeroSection() {
   return (
     <section className="min-h-screen">
       <div className="min-h-[90dvh] flex flex-wrap md:flex-nowrap justify-between  gap-4 ">
-        <div className="w-full md:w-6/12 px-4 md:px-12 flex flex-col justify-center items-start  gap-12">
+        <div className="w-full md:w-6/12 px-4 md:px-12 flex flex-col justify-center items-start pt-24 md:pt-20  gap-12">
           <h1 className="text-3xl md:text-5xl text-bts-GreenOne font-bold">
             Partnership that Empower
           </h1>
@@ -14,14 +15,33 @@ export default function HeroSection() {
             We collaborate with organizations committed to empowering global
             talent and transforming remote opportunities into thriving careers.
           </p>
-          <Link href="mailto:info@beyondthesavannah.co.ke">
-            <Button
-              size="lg"
-              className="text-base border-bts-BrownThree bg-bts-BrownThree text-black hover:bg-bts-BrownFour hover:text-white hover:scale-105 transition duration-500"
-            >
-              Start the Conversation
-            </Button>
-          </Link>
+        
+           <Dialog>
+              <DialogTrigger asChild>
+                <Link
+                  href="https://calendly.com/beyondthesavannah-info/partner-discovery-call"
+                  target="partners-calendarly-page-show"
+                >
+                  <Button
+                    size="lg"
+                    className="text-base border-bts-BrownThree bg-bts-BrownThree text-black hover:bg-bts-BrownFour hover:text-white hover:scale-105 transition duration-500"
+                  >
+                    Start The Conversation
+                  </Button>
+                </Link>
+              </DialogTrigger>
+              <DialogContent className="min-h-[80dvh] w-[80vw]">
+                <DialogHeader>
+                  <DialogTitle></DialogTitle>
+                  <DialogDescription></DialogDescription>
+                  <iframe
+                    name="partners-calendarly-page-show"
+                    src="https://calendly.com/beyondthesavannah-info/partner-discovery-call"
+                    className="h-full w-full"
+                  ></iframe>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
         </div>
         <div className="w-full md:w-6/12 -z-10">
           <DisplayImageFromNextCloudinary
