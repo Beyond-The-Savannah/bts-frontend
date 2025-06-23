e6ad51a47de6cb8b9389b9755941a5cc2b576ee9
Michael Gitonga <gitoshmbae@gmail.com>
2025-06-23 15:48:28 +0300

    Adds a check for userSubscription information to view a job details in src/app/Customer/find-jobs/[jobsId]/page.tsx

--- src/app/Customer/find-jobs/[jobsId]/page.tsx
+++ src/app/Customer/find-jobs/[jobsId]/page.tsx
@@ -1,4 +1,7 @@
+import { GetUserSubscriptionInformation } from "@/components/Customer/UserSubscriptionInformation";
 import ViewJob from "@/components/Customer/ViewJob";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function page({
   params,
@@ -6,9 +9,24 @@ export default async function page({
   params: Promise<{ jobsId: string }>;
 }) {
   const jobsId = (await params).jobsId;
+  const userSubscriptionInformation = await GetUserSubscriptionInformation();
+  const user = await currentUser();
+
+  const byPassEmailAddress = "anngachanja15@gmail.com";
+
+  if (
+    (userSubscriptionInformation == null ||
+      userSubscriptionInformation == undefined) &&
+    byPassEmailAddress != user?.emailAddresses[0].emailAddress
+  ) {
+    redirect("/Customer");
+  }
   return (
     <>
       <ViewJob jobsId={jobsId} />
     </>
   );
 }
+
+// http://localhost:3000/Customer/find-jobs/3010
+// http://localhost:3000/Customer/find-jobs/3009, 3008,3007
