5d662dd8fe538f03c5313307c4ad1a9a44ba108f
Michael Gitonga <gitoshmbae@gmail.com>
2025-07-18 13:02:47 +0300

    Add a service policy to the services entries in src/staticData/services.ts Updates the copy of the services in src/staticData/services.ts, Adjust the UI of the single service view to be more responsive in src/app/(root)/service/[titleSlug]/page.tsx

--- src/app/(root)/service/[titleSlug]/page.tsx
+++ src/app/(root)/service/[titleSlug]/page.tsx
@@ -60,9 +60,10 @@ export default async function ServicePage({
   await posthog?.shutdown();
   return (
     <>
-      <section className="container mx-auto space-y-16 px-4 pt-24 md:pt-48 pb-40">
-        <div className="min-h-[50vh]  flex flex-wrap justify-around gap-y-12 lg:gap-x-8">
-          <div className="w-[85vw] md:w-[45vw] lg:max-w-[34vw] ">
+      <section className="container mx-auto space-y-16 px-4 pt-24 md:pt-36 lg:pb-40">
+        <div className="min-h-[50vh]  flex flex-wrap lg:flex-nowrap justify-around gap-y-12 gap-x-12">
+          {/* <div className="w-[85vw] md:w-[45vw] lg:max-w-[34vw] "> */}
+          <div className="w-full lg:max-w-[34vw] ">
             {specificService && (
               <DisplayImageFromNextCloudinary
                 src={specificService.img}
@@ -70,24 +71,28 @@ export default async function ServicePage({
                 width={800}
                 prioty
                 alt={`beyond the savannah, ${specificService.title} service image`}
-                classname="object-cover w-[40rem] bg-bts-BrownOne h-[40vh] rounded-xl"
+                classname="object-cover w-full lg:w-[40rem] mx-auto  bg-bts-BrownOne h-[40vh] rounded-xl"
               />
             )}
           </div>
-          <div className=" w-[85vw] md:w-[44vw] lg:w-[48vw] space-y-4 text-balancee">
-            <h1 className=" text-2xl xl:text-3xl lg:text-5xl  text-bts-GreenOne font-bold">
-              {specificService?.title}
-            </h1>
-            <p className="bg-amber-100 rounded-lg py-2 px-6 w-4/12 text">
-              {" "}
-              KES{" "}
-              <span className="text-2xl">{specificService?.priceString}</span>
-              {serviceTitleSlug == "beyond-the-savannah-whatsApp-community" ? (
-                <>
-                  <span>/annually</span>
-                </>
-              ) : null}
-            </p>
+          {/* <div className=" w-[85vw] md:w-[44vw] lg:w-[48vw] space-y-4 text-balancee"> */}
+          <div className=" w-[85vw]  lg:w-[48vw] space-y-4 ">
+            <div className="flex lg:flex-col flex-wrap items-center lg:items-start gap-4">
+              <h1 className=" text-2xl xl:text-3xl lg:text-5xl  text-bts-GreenOne font-bold">
+                {specificService?.title}
+              </h1>
+              <p className="bg-amber-100 rounded-lg py-2 px-6 w-6/12 md:w-auto text">
+                {" "}
+                KES{" "}
+                <span className="text-2xl">{specificService?.priceString}</span>
+                {serviceTitleSlug == "beyond-the-savannah-whatsApp-community" ? (
+                  <>
+                    <span>/annually</span>
+                  </>
+                ) : null}
+              </p>
+
+            </div>
             <p className="text-2xl font-medium lg:text-xl w-full">
               {specificService?.subheading}
             </p>
@@ -95,7 +100,8 @@ export default async function ServicePage({
           </div>
         </div>
 
-        <div className="min-h-[50vh]  flex flex-wrap justify-around gap-y-12">
+        {/* <div className="min-h-[50vh]  flex flex-wrap justify-around gap-12"> */}
+        <div className="min-h-[50vh]  flex flex-wrap md:flex-nowrap justify-between gap-12 pt-10">
           <div className="w-[85vw] md:max-w-[50vw] ">
             <div className="flex items-center justify-between ">
               <div className="c">
@@ -140,7 +146,11 @@ export default async function ServicePage({
                   asChild
                   className="px-4 py-2 rounded-lg w-full text-white bg-bts-GreenOne hover:bg-green-700 hover:shadow-bts-BrownThree hover:shadow-md duration-700"
                 >
-                  <Link href={`/Customer/whatsappService?source=whatsapp-service`}>Get Service</Link>
+                  <Link
+                    href={`/Customer/whatsappService?source=whatsapp-service`}
+                  >
+                    Get Service
+                  </Link>
                 </Button>
               </>
             ) : (
@@ -156,6 +166,18 @@ export default async function ServicePage({
                         Please enter the following details, and choose your
                         preffered payment method to complete the service
                         purchase
+                        {specificService?.servicePolicy != "" && (
+                          <span className="c rounded-lg bg-amber-50 px-3 py-6 flex flex-col gap-2 my-4 items-start">
+                            <AlertCircle className="size-4" />
+                            <span className="text-xs">
+                              Please note: By proceeding with payment for this
+                              service and booking a subsequent session, you
+                              agree that the session is non-refundable if you do
+                              not attend your booked time slot. We do not offer
+                              rescheduling for no-shows.
+                            </span>
+                          </span>
+                        )}
                       </DrawerDescription>
                     </DrawerHeader>
                     <div className=" max-w-md mx-auto py-4 px-8">
--- src/staticData/services.ts
+++ src/staticData/services.ts
@@ -4,20 +4,22 @@ export const servicesList = [
     title: "LinkedIn Optimization",
     titleSlug: "linkedin-optimization",
     subheading:
-      "Elevate Your Professional Brand with a 15-Minute LinkedIn Profile Optimization Session",
+      "Elevate Your Professional Brand with a 15 Minute LinkedIn Profile Optimization Session",
     img: "linkedin_website_view_1_kz1qml",
     openGraphImg: "LinkedIn_Optimization_openGraph_egoqgl",
     details:
-      "Your LinkedIn profile is your digital business card, and it has the power to make a lasting impression on potential employers, clients, and professional connections. Join us for a focused 15-minute virtual consultation designed to revamp and optimize your LinkedIn profile for success.",
+      "Your LinkedIn profile is your digital business card, and it has the power to make a lasting impression on potential employers, clients, and professional connections. Join us for a focused 15 minute virtual consultation designed to revamp and optimize your LinkedIn profile for success.",
     benefits: [
       "Tailored Recommendations: Receive personalized insights and recommendations from an experienced professional.",
       "Profile Enhancements: Discover strategies to boost your visibility and credibility on the world's largest professional network.",
-      "Keyword Optimization: Learn how to strategically incorporate industry-relevant keywords into your profile to attract the right opportunities.",
+      "Keyword Optimization: Learn how to strategically incorporate industry relevant keywords into your profile to attract the right opportunities.",
       "Networking Tips: Unlock the secrets to expanding your network and maximizing your connections.",
       "Actionable Steps: Walk away with actionable steps and a clearer understanding of how to leverage LinkedIn effectively.",
     ],
+    servicePolicy:
+      "Please note: By proceeding with payment for this service and booking a subsequent session, you agree that the session fee is non-refundable if you do not attend your booked time slot. We do not offer rescheduling for no-shows.",
     valueProposal:
-      "Invest in your professional growth and make the most out of your LinkedIn presence. Book your 15-minute LinkedIn profile optimization session today and watch your online brand thrive!",
+      "Invest in your professional growth and make the most out of your LinkedIn presence. Book your 15 minute LinkedIn profile optimization session today and watch your online brand thrive!",
     price: 15000,
     priceString: "15,000",
   },
@@ -36,6 +38,8 @@ export const servicesList = [
       "Cover Letter: Craft a compelling cover letter that highlights your strengths and showcases your motivation. We'll help you create a personalized cover letter that makes a strong impression.",
       "Bonus: As a special bonus, we're offering insights into three job openings currently available, giving you a head start in your job search.",
     ],
+    servicePolicy:
+      "Please note: By proceeding with payment for this service and booking a subsequent session, you agree that the session fee is non-refundable if you do not attend your booked time slot. We do not offer rescheduling for no-shows.",
     valueProposal:
       "This is the boost your career needs. Take action today to increase your chances of landing your dream job. Book our CV review and cover letter package now",
     price: 5000,
@@ -49,11 +53,13 @@ export const servicesList = [
     img: "avs_cv_ymup2h",
     openGraphImg: "ATS_CV_Revamp_openGraph_v2dhgx",
     details:
-      "Navigating the Applicant Tracking System (ATS) is crucial in today's competitive job market, and we're here to help you excel. Our comprehensive package includes an ATS-compliant CV and a precision-crafted ATS-compliant cover letter, tailored to your specific job application needs.",
+      "Navigating the Applicant Tracking System (ATS) is crucial in today's competitive job market, and we're here to help you excel. Our comprehensive package includes an ATS compliant CV and a precision crafted ATS compliant cover letter, tailored to your specific job application needs.",
     benefits: [
-      "ATS-Compliant CV: We'll create a CV that passes through ATS filters with ease, ensuring your qualifications and experience are properly recognized by employers.",
-      "ATS-Compliant Cover Letter: Craft a cover letter that complements your CV and grabs the attention of hiring managers. We'll make sure it meets ATS standards and showcases your motivation.",
+      "ATS Compliant CV: We'll create a CV that passes through ATS filters with ease, ensuring your qualifications and experience are properly recognized by employers.",
+      "ATS Compliant Cover Letter: Craft a cover letter that complements your CV and grabs the attention of hiring managers. We'll make sure it meets ATS standards and showcases your motivation.",
     ],
+    servicePolicy:
+      "Please note: By proceeding with payment for this service and booking a subsequent session, you agree that the session fee is non-refundable if you do not attend your booked time slot. We do not offer rescheduling for no-shows.",
     valueProposal:
       "This is the boost your career needs. Take action today to increase your chances of landing your dream job. Book our ATS CV revamp and cover letter package now!",
     price: 8000,
@@ -67,7 +73,7 @@ export const servicesList = [
     img: "coaching_ekq5bx",
     openGraphImg: "Coaching_Session_openGraph_znalhl",
     details:
-      "Embark on a journey of self-discovery, self-assessment, and goal setting that will transform the way you approach your career and personal growth.",
+      "Embark on a journey of self discovery, self assessment, and goal setting that will transform the way you approach your career and personal growth.",
     benefits: [
       "Discover Your True Self: Gain insights into your strengths, values, and aspirations to better understand your unique path.",
       "Identify Challenges: Pinpoint any obstacles or challenges that may be holding you back from reaching your full potential.",
@@ -75,6 +81,8 @@ export const servicesList = [
       "Set Clear Goals: Define concrete, achievable goals that align with your ambitions.",
       "Chart Your Course: Create a roadmap for your personal and professional journey.",
     ],
+    servicePolicy:
+      "Please note: By proceeding with payment for this service and booking a subsequent session, you agree that the session fee is non-refundable if you do not attend your booked time slot. We do not offer rescheduling for no-shows.",
     valueProposal:
       "Invest in yourself and take the first step toward a brighter, more fulfilling future. Book your 1-hour coaching session today to start your transformative journey!",
     price: 7000,
@@ -85,11 +93,11 @@ export const servicesList = [
     title: "Interview Preparation",
     titleSlug: "interview-preparation",
     subheading:
-      "Prepare for Success with a 2-Hour Guided Interview Preparation Session",
+      "Prepare for Success with a 2 Hour Guided Interview Preparation Session",
     img: "interview_prep_wi4ubt",
     openGraphImg: "Interview_Preparation_openGraph_czck60",
     details:
-      "Your next career move deserves careful planning and strategy. Join us for a comprehensive 2-hour virtual meeting dedicated to interview preparation, where we'll equip you with the tools and confidence you need to succeed.",
+      "Your next career move deserves careful planning and strategy. Join us for a comprehensive 2 hour virtual meeting dedicated to interview preparation, where we'll equip you with the tools and confidence you need to succeed.",
     benefits: [
       "Discover Your True Self: Gain insights into your strengths, values, and aspirations to better understand your unique path.",
       "Personalized Guidance: Tailored interview coaching that addresses your specific strengths and areas of improvement.",
@@ -98,6 +106,8 @@ export const servicesList = [
       "Feedback and Action Plans: Receive constructive feedback and actionable steps to enhance your interview performance.",
       "Your Path to Success: Chart your unique path to interview success, no matter your industry or role.",
     ],
+    servicePolicy:
+      "Please note: By proceeding with payment for this service and booking a subsequent session, you agree that the session fee is non-refundable if you do not attend your booked time slot. We do not offer rescheduling for no-shows.",
     valueProposal:
       "Invest in yourself and take the next step toward landing your dream job. Book your 2-hour guided interview preparation session today and set yourself up for a brighter career future!",
     price: 12000,
@@ -112,7 +122,7 @@ export const servicesList = [
     img: "consultation_qiod4j",
     openGraphImg: "Consultation_Call_openGraph_cecn9j",
     details:
-      "Are you feeling stuck in your career or looking for tailored guidance to elevate your professional journey? this is more than a call—it's your opportunity to invest in yourself and get expert-level guidance. Ready to take the next step? Let's talk!",
+      "Are you feeling stuck in your career or looking for tailored guidance to elevate your professional journey? this is more than a call, it's your opportunity to invest in yourself and get expert level guidance. Ready to take the next step? Let's talk!",
     benefits: [
       "Receive a personalized plan that aligns with your remote work goals—whether you're transitioning into remote work, seeking a new remote role, or aiming to excel in your current position.",
       "Gain access to expert tips on how to effectively search for and secure remote job opportunities that fit your skill set and lifestyle. Learn how to stand out in the competitive remote work market.",
@@ -121,6 +131,8 @@ export const servicesList = [
       "Stay ahead with the latest trends and changes in the remote work sector, so you can adapt to new technologies, industries, and opportunities that emerge in the evolving remote workforce.",
       "Ask questions and seek guidance on anything remote working.",
     ],
+    servicePolicy:
+      "Please note: By proceeding with payment for this service and booking a subsequent session, you agree that the session fee is non-refundable if you do not attend your booked time slot. We do not offer rescheduling for no-shows.",
     valueProposal:
       "Are you feeling stuck in your career or unsure about your next steps? Let us have an one-on-one and provide tailored advice and actionable strategies to help you navigate your professional challenges.",
     price: 4000,
@@ -134,14 +146,15 @@ export const servicesList = [
     img: "remote_master_class_xmnolp",
     openGraphImg: "Remote_Work_Master_Class_openGraph_b8wp3v",
     details:
-      "Ready to thrive in the world of remote work? Join our exclusive Master Class designed for professionals seeking to transition to or excel in remote roles. Whether you're a seasoned remote worker or just starting, don't miss your chance to elevate your career—register now!",
+      "Ready to thrive in the world of remote work? Join our exclusive Master Class designed for professionals seeking to transition to or excel in remote roles. Whether you're a seasoned remote worker or just starting, don't miss your chance to elevate your career,register now!",
     benefits: [
       "Gain a clear understanding of remote job basics, different employment types, key  and the optimal time zones to target for job applications.",
-      "Explore the fastest-growing remote roles, the importance of upskilling, and the best platforms to enhance your skills.",
+      "Explore the fastest growing remote roles, the importance of upskilling, and the best platforms to enhance your skills.",
       "Learn what recruiters value most and discover how to distinguish yourself from other candidate.",
       "Find reliable remote job opportunities, effectively navigate remote job boards, and recognize red flags to avoid scams.",
       "Learn from the industry expert with years of experience in remote work.",
     ],
+    servicePolicy: "",
     valueProposal:
       "Whether you're a seasoned professional looking to transition to remote roles or a newcomer eager to enter the workforce, our Master Class is designed to equip you with the essential skills, strategies, and insights needed to thrive in a work environment.",
     price: 3500,
@@ -158,10 +171,12 @@ export const servicesList = [
       "In the dynamic landscape of remote work, establishing a personal connection can be challenging. This is why most if not all companies, require a 1 minute introductory video. This package will give you pointers and how to address yourself to your future employer!",
     benefits: [
       "Creating a Personal Connection: Get help in introducing yourself —through tone, body language, and expression. This is essential in the remote world where face-to-face interactions are limited.",
-      "Establishing Credibility:In the absence of in-person meetings, a video provides a platform to showcase professionalism, expertise, and personality. This package covers this from end to end.",
+      "Establishing Credibility:In the absence of in person meetings, a video provides a platform to showcase professionalism, expertise, and personality. This package covers this from end to end.",
       "Clear Communication of Expectations: Get a how to introductory video that will allow you to clearly articulate their roles, work styles, and expectations.",
       "Engagement and Accessibility: Get a 15 minutes consultation call for pointers and one round of review.",
     ],
+    servicePolicy:
+      "Please note: By proceeding with payment for this service and booking a subsequent session, you agree that the session fee is non-refundable if you do not attend your booked time slot. We do not offer rescheduling for no-shows.",
     valueProposal:
       "An introductory video is more than just a welcome, it's an essential tool for remote workers to establish rapport, communicate effectively, and set the tone for a productive working relationship.",
     price: 3000,
@@ -183,10 +198,12 @@ export const servicesList = [
       "LinkedIn Optimization - We'll transform your LinkedIn profile into a magnet for job opportunities, ensuring it ranks higher in recruiter searches + 15 minutes consultation call",
       "Introductory Video - Pointers on how to make a polished video to introduce yourself professionally, making you memorable to potential employers.",
     ],
+    servicePolicy:
+      "Please note: By proceeding with payment for this service and booking a subsequent session, you agree that the session fee is non-refundable if you do not attend your booked time slot. We do not offer rescheduling for no-shows.",
     valueProposal:
       "With our Remote Work Starter Bundle, you'll gain the edge you need to succeed in today's worldwide job landscape.",
-      // "Easter is here and we have a basket full of goodies! Enjoy the same package at a slashed price of 20,000 instead of 26,000! Take advantage of this offer running until the end of April.",
-      // "This season, we're honoring the bold, the brave, and the visionary founders across the globe who dared to start. In the spirit of freedom and building something from scratch, we're offering an exclusive discount on our services to support your journey.",
+    // "Easter is here and we have a basket full of goodies! Enjoy the same package at a slashed price of 20,000 instead of 26,000! Take advantage of this offer running until the end of April.",
+    // "This season, we're honoring the bold, the brave, and the visionary founders across the globe who dared to start. In the spirit of freedom and building something from scratch, we're offering an exclusive discount on our services to support your journey.",
     price: 26000,
     priceString: "26,000",
     // price: 20000,
@@ -200,12 +217,13 @@ export const servicesList = [
     img: "whatsapp_community_img_uwcvhd",
     openGraphImg: "whatsapp_community_img_uwcvhd",
     details:
-      "Struggling to navigate the tough job market? You're not alone. Join our exclusive paid WhatsApp community—a safe space for job seekers and professionals looking for support, motivation, and expert guidance on securing remote jobs. ",
+      "Struggling to navigate the tough job market? You're not alone. Join our exclusive paid WhatsApp community, a safe space for job seekers and professionals looking for support, motivation, and expert guidance on securing remote jobs. ",
     benefits: [
       "Encouragement & Accountability - Stay motivated with daily insights and success stories.",
       "Networking & Community - Connect with like-minded professionals and expand your opportunities.",
       "Q&A with Experts - Get answers to your job-related questions from seasoned professionals.",
     ],
+    servicePolicy: "",
     valueProposal:
       "Don't struggle in silence— let's navigate this journey together! Ready to invest in your career success? ",
     price: 6000,
