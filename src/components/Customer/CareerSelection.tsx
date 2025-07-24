241bb0cb258ffdb6f84e750be9a4d5f73da1da2d
Michael Gitonga <gitoshmbae@gmail.com>
2025-07-24 11:43:16 +0300

    Adds a Resume Upload Component in src/components/Customer/ReusmeUpload1.tsx, Increases the min height of the CareerSelection component in src/components/Customer/CareerSelection.tsx

--- src/components/Customer/CareerSelection.tsx
+++ src/components/Customer/CareerSelection.tsx
@@ -5,7 +5,7 @@ export default function CareerSelection({emailAddress}:{emailAddress:string}) {
     <section className="py-10">
       {/* <div className="min-h-[30vh] space-y-6 px-4 py-8 bg-bts-BrownOne/50 rounded-lg w-full lg:w-[30rem]"> */}
       {/* <div className="min-h-[30vh] space-y-6 ml-0 md:ml-8 px-4 py-8 bg-bts-BrownOne/50 rounded-lg w-full lg:w-4/12"> */}
-      <div className="min-h-[30vh] space-y-6 ml-0 lg:ml-8 px-4 py-8 bg-bts-BrownOne/50 rounded-lg w-full lg:w-9/12">
+      <div className="min-h-[35vh] space-y-6 ml-0 lg:ml-8 px-4 py-8 bg-bts-BrownOne/50 rounded-lg w-full lg:w-9/12">
         <div className="space-y-4">
           <p className="font-semibold text-xl">Career Selection</p>
         </div>
