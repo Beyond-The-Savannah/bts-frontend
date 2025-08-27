c76fec2a30363f01ff9433fb6d5b787ba566a13f
Michael Gitonga <gitoshmbae@gmail.com>
2025-08-27 10:38:04 +0300

    Adds a DashboardPageLoader component in src/components/Loaders/DashboardPageLoader.tsx, Updates the Admin & Customer loaders to use the DashboardPageLoader

--- src/app/Customer/loading.tsx
+++ src/app/Customer/loading.tsx
@@ -1,7 +1,9 @@
+import DashboardPageLoader from "@/components/Loaders/DashboardPageLoader";
+
 export default function Loading() {
   return (
     <>
-      <section className="min-h-screen w-full  px-4">
+      {/* <section className="min-h-screen w-full  px-4">
         <div className=" min-h-screen container mr-auto space-y-12">
           <div className="w-96 h-24 rounded-lg bg-bts-BrownOne/50   animate-pulse ">
             <div className="pt-6 pl-4 space-y-2">
@@ -12,7 +14,8 @@ export default function Loading() {
             <div className="bg-bts-BrownOne/50 rounded-lg animate-pulse w-full lg:w-[80dvw] min-h-[75dvh]"></div>
           </div>
         </div>
-      </section>
+      </section> */}
+      <DashboardPageLoader/>
     </>
   );
 }
--- src/components/Loaders/DashboardPageLoader.tsx
+++ src/components/Loaders/DashboardPageLoader.tsx
@@ -1,21 +1,18 @@
-import DashboardPageLoader from "@/components/Loaders/DashboardPageLoader";
-
-export default function Loading() {
+export default function DashboardPageLoader() {
   return (
     <>
-      {/* <section className="min-h-screen w-full px-4">
+      <section className="min-h-screen w-full  px-4">
         <div className=" min-h-screen container mr-auto space-y-12">
-          <div className="w-96 h-24 rounded-lg bg-bts-BrownOne/50  animate-pulse ">
+          <div className="w-96 h-24 rounded-lg bg-bts-BrownOne/50   animate-pulse ">
             <div className="pt-6 pl-4 space-y-2">
               <div className="w-10/12 h-12 rounded bg-bts-BrownOne"></div>
             </div>
           </div>
-          <div className="w-full flex flex-wrap justify-stretch gap-x-6 gap-y-12 mt-10">
+          <div className="w-full flex flex-wrap  justify-stretch gap-x-6 gap-y-12 mt-10">
             <div className="bg-bts-BrownOne/50 rounded-lg animate-pulse w-full lg:w-[80dvw] min-h-[75dvh]"></div>
           </div>
         </div>
-      </section> */}
-      <DashboardPageLoader/>
+      </section>
     </>
   );
 }
