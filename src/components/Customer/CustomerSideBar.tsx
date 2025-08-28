c8b4831bc12a9c557a2b0c137d9d949243745bbc
Michael Gitonga <gitoshmbae@gmail.com>
2025-08-28 14:58:36 +0300

    Comments out the code in src/components/Customer/CustomerSideBar.tsx which is't being used as we have a new component CustomerSideBar2

--- src/components/Customer/CustomerSideBar.tsx
+++ src/components/Customer/CustomerSideBar.tsx
@@ -1,139 +1,139 @@
-import { FileStack, FolderPlus, MessagesSquare, Server } from "lucide-react";
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "../ui/sidebar";
-import { Link } from "next-view-transitions";
-import { SignedIn, UserButton } from "@clerk/nextjs";
+// import { FileStack, FolderPlus, MessagesSquare, Server } from "lucide-react";
+// import {
+//   Sidebar,
+//   SidebarContent,
+//   SidebarGroup,
+//   SidebarGroupContent,
+//   SidebarGroupLabel,
+//   SidebarMenu,
+//   SidebarMenuButton,
+//   SidebarMenuItem,
+// } from "../ui/sidebar";
+// import { Link } from "next-view-transitions";
+// import { SignedIn, UserButton } from "@clerk/nextjs";
 
-import { GetUserSubscriptionInformation } from "./UserSubscriptionInformation";
-import { currentUser } from "@clerk/nextjs/server";
+// import { GetUserSubscriptionInformation } from "./UserSubscriptionInformation";
+// import { currentUser } from "@clerk/nextjs/server";
 
- const byPassEmailAddresses = [
-    `onyango.mary15@gmail.com`,
-    `kimothoevalyne@gmail.com`,
-    `thothocaroline@gmail.com`,
+//  const byPassEmailAddresses = [
+//     `onyango.mary15@gmail.com`,
+//     `kimothoevalyne@gmail.com`,
+//     `thothocaroline@gmail.com`,
     
-  ];
+//   ];
 
-const items = [
-  {
-    title: "Jobs",
-    url: "/Customer/find-jobs",
-    icon: FileStack,
-  },
-  {
-    title: "Subscription",
-    url: "/Customer/",
-    icon: FolderPlus,
-  },
-];
-const items2 = [
-  {
-    title: "Whatsapp Subscription",
-    url: "/Customer/whatsappService?source=whatsapp-service",
-    icon: FolderPlus,
-  },
-];
-const items3 = [
-  {
-    title: "Services",
-    // url: "/#services",
-    url: "/Customer/btsServices",
-    icon: Server,
-  },
-  {
-    title: "Savannah Assistant",
-    url: "/Customer/Savannah",
-    icon: MessagesSquare,
-  },
-];
-export default async function CustomerSideBar() {
-  const userSubscriptionInformation = await GetUserSubscriptionInformation();
-  const user = await currentUser();
-  // console.log("USEINFO SIDEBAR", userSubscriptionInformation);
+// const items = [
+//   {
+//     title: "Jobs",
+//     url: "/Customer/find-jobs",
+//     icon: FileStack,
+//   },
+//   {
+//     title: "Subscription",
+//     url: "/Customer/",
+//     icon: FolderPlus,
+//   },
+// ];
+// const items2 = [
+//   {
+//     title: "Whatsapp Subscription",
+//     url: "/Customer/whatsappService?source=whatsapp-service",
+//     icon: FolderPlus,
+//   },
+// ];
+// const items3 = [
+//   {
+//     title: "Services",
+//     // url: "/#services",
+//     url: "/Customer/btsServices",
+//     icon: Server,
+//   },
+//   {
+//     title: "Savannah Assistant",
+//     url: "/Customer/Savannah",
+//     icon: MessagesSquare,
+//   },
+// ];
+// export default async function CustomerSideBar() {
+//   const userSubscriptionInformation = await GetUserSubscriptionInformation();
+//   const user = await currentUser();
+//   // console.log("USEINFO SIDEBAR", userSubscriptionInformation);
 
-  return (
-    <Sidebar>
-      <SidebarContent className="bg-bts-GreenOne text-white">
-        <SidebarGroup>
-          <SidebarGroupLabel></SidebarGroupLabel>
-          {/* <SidebarGroupContent className="flex flex-col justify-between h-[90vh]"> */}
-          <SidebarGroupContent className="flex flex-col justify-between min-h-[90dvh]">
-            <SidebarMenu className="space-y-2">
+//   return (
+//     <Sidebar>
+//       <SidebarContent className="bg-bts-GreenOne text-white">
+//         <SidebarGroup>
+//           <SidebarGroupLabel></SidebarGroupLabel>
+//           {/* <SidebarGroupContent className="flex flex-col justify-between h-[90vh]"> */}
+//           <SidebarGroupContent className="flex flex-col justify-between min-h-[90dvh]">
+//             <SidebarMenu className="space-y-2">
               
-              {
+//               {
               
-              (["active", "attention", "non-renewing", "completed"].includes(userSubscriptionInformation?.status as string) && userSubscriptionInformation?.plan?.amount != 600000 ) ||
-              (userSubscriptionInformation==null || userSubscriptionInformation==undefined) && byPassEmailAddresses.includes(user?.emailAddresses[0].emailAddress as string)
-              ? 
+//               (["active", "attention", "non-renewing", "completed"].includes(userSubscriptionInformation?.status as string) && userSubscriptionInformation?.plan?.amount != 600000 ) ||
+//               (userSubscriptionInformation==null || userSubscriptionInformation==undefined) && byPassEmailAddresses.includes(user?.emailAddresses[0].emailAddress as string)
+//               ? 
               
-              (
-                <>
-                  {" "}
-                  {items.map((item) => (
-                    <SidebarMenuItem key={item.title}>
-                      <SidebarMenuButton asChild>
-                        <Link href={item.url}>
-                          <item.icon />
-                          <span>{item.title}</span>
-                        </Link>
-                      </SidebarMenuButton>
-                    </SidebarMenuItem>
-                  ))}
-                </>
-              ) : null}
-              {
-              ["active", "attention", "non-renewing", "completed"].includes(
-                userSubscriptionInformation?.status as string
-              ) && userSubscriptionInformation?.plan?.amount == 600000 
+//               (
+//                 <>
+//                   {" "}
+//                   {items.map((item) => (
+//                     <SidebarMenuItem key={item.title}>
+//                       <SidebarMenuButton asChild>
+//                         <Link href={item.url}>
+//                           <item.icon />
+//                           <span>{item.title}</span>
+//                         </Link>
+//                       </SidebarMenuButton>
+//                     </SidebarMenuItem>
+//                   ))}
+//                 </>
+//               ) : null}
+//               {
+//               ["active", "attention", "non-renewing", "completed"].includes(
+//                 userSubscriptionInformation?.status as string
+//               ) && userSubscriptionInformation?.plan?.amount == 600000 
           
-              ? 
+//               ? 
               
-              (
-                <>
-                  {items2.map((item) => (
-                    <SidebarMenuItem key={item.title}>
-                      <SidebarMenuButton asChild>
-                        <Link href={item.url}>
-                          <item.icon />
-                          <span>{item.title}</span>
-                        </Link>
-                      </SidebarMenuButton>
-                    </SidebarMenuItem>
-                  ))}
-                </>
-              ) : null}
-              <>
-                {items3.map((item) => (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild>
-                      <Link href={item.url}>
-                        <item.icon />
-                        <span>{item.title}</span>
-                      </Link>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </>
-            </SidebarMenu>
-            <SidebarMenu>
-              <SignedIn>
-                <div className="bg-bts-BrownThree/5 p-1 rounded-2xl flex gap-1 items-center">
-                  <p>Signed In as </p>
-                  <UserButton />
-                </div>
-              </SignedIn>
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-    </Sidebar>
-  );
-}
+//               (
+//                 <>
+//                   {items2.map((item) => (
+//                     <SidebarMenuItem key={item.title}>
+//                       <SidebarMenuButton asChild>
+//                         <Link href={item.url}>
+//                           <item.icon />
+//                           <span>{item.title}</span>
+//                         </Link>
+//                       </SidebarMenuButton>
+//                     </SidebarMenuItem>
+//                   ))}
+//                 </>
+//               ) : null}
+//               <>
+//                 {items3.map((item) => (
+//                   <SidebarMenuItem key={item.title}>
+//                     <SidebarMenuButton asChild>
+//                       <Link href={item.url}>
+//                         <item.icon />
+//                         <span>{item.title}</span>
+//                       </Link>
+//                     </SidebarMenuButton>
+//                   </SidebarMenuItem>
+//                 ))}
+//               </>
+//             </SidebarMenu>
+//             <SidebarMenu>
+//               <SignedIn>
+//                 <div className="bg-bts-BrownThree/5 p-1 rounded-2xl flex gap-1 items-center">
+//                   <p>Signed In as </p>
+//                   <UserButton />
+//                 </div>
+//               </SignedIn>
+//             </SidebarMenu>
+//           </SidebarGroupContent>
+//         </SidebarGroup>
+//       </SidebarContent>
+//     </Sidebar>
+//   );
+// }
