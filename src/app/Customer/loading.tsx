0d0dc6925688d66392af91c6c7046e4fd1a84686
Michael Gitonga <gitoshmbae@gmail.com>
2025-09-08 10:05:36 +0300

    Readds the loading.tsx file

---
+++ src/app/Customer/loading.tsx
@@ -0,0 +1,9 @@
+import DashboardPageLoader from "@/components/Loaders/DashboardPageLoader";
+
+export default function Loading() {
+  return (
+    <>
+      <DashboardPageLoader/>
+    </>
+  );
+}
