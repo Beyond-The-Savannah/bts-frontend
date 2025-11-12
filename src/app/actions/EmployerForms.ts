cc44583a1ff0de1811edac6a351b4f76da37aff0
Michael Gitonga <gitoshmbae@gmail.com>
2025-11-12 17:14:56 +0300

    Adds a new plugin to render the html content using ReactMarkDown in src/app/Employer/jobs/page.tsx, Updates the job entries schema in src/db/schema.ts, Adds new select input values in src/staticData/Employer/entries.ts, Updates the AddJobsForm to use comboBox for the selects, adds hidden inputs as well in src/components/Employer/JobSection.tsx/AddJobsForm.tsx, Adds a tabs component to show jobs and the addJobForm in src/app/Employer/jobs/page.tsx

--- src/app/actions/EmployerForms.ts
+++ src/app/actions/EmployerForms.ts
@@ -1,23 +1,23 @@
 "use server";
 
 import { db } from "@/db/db";
-import { jobsTable } from "@/db/schema";
+import {  JobsProp, jobsTable } from "@/db/schema";
 
-// export async function AddNewJobForm(data:Omit<JobsProp, 'id'|'createdAt'|'updatedAt'>) {
-export async function AddNewJobForm(data: { deadLineDate: string; companyName: string; role: string; workMode: string; department: string; jobDetails: string; }) {
+export async function AddNewJobForm(data:Omit<JobsProp, 'id'|'createdAt'|'updatedAt'>) {
+// export async function AddNewJobForm(data: { deadLine: string; companyName: string; role: string; workMode: string; department: string; jobDetails: string; }) {
   try {
    await db.insert(jobsTable).values({
       role: data.role,
+      companyName:data.companyName,
       workMode: data.workMode,
-      jobType: "Full Time",
       department: data.department,
-      author: "current-loged-user",
-      deadLine: data.deadLineDate,
       jobDetails: data.jobDetails,
+      jobType:data.jobType,
+      author: data.author,
+      deadLine: data.deadLine,
       applicationLink: "",
     })
   } catch (error) {
     console.log("Error in AddNewJobForm -Employer Section", error);
   }
 }
-
