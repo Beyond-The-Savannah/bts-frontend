42cc172ef2187ae4a6c8d76dd4fb7f49ba0bb1da
Michael Gitonga <gitoshmbae@gmail.com>
2025-11-24 12:57:27 +0300

    Uses promise.all over the foreach loop for efficiecny in src/components/Customer/UserSubscriptionInformation.ts, Updates the records perpage to 100 in both  src/app/api/subscriptions/route.ts && src/app/api/subscription-details-by-plan-codes/route.ts

--- src/components/Customer/UserSubscriptionInformation.ts
+++ src/components/Customer/UserSubscriptionInformation.ts
@@ -1,10 +1,10 @@
-// import { AddSubscriberEmailToMailerLite } from "@/lib/mailerLite";
+
 import { AddSubscriberToKit } from "@/lib/kitNewsLetter";
 import { SubscriptionProps } from "@/types/subscriptions";
 import { currentUser } from "@clerk/nextjs/server";
 
-// const PUBLIC_BASE_URL = process.env.PUBLIC_BASE_URL;
-const BTS_API_URL = process.env.NEXT_PUBLIC_DB_BASE_URL;
+const PUBLIC_BASE_URL = process.env.PUBLIC_BASE_URL;
+// const BTS_API_URL = process.env.NEXT_PUBLIC_DB_BASE_URL;
 
 export async function GetUserSubscriptionInformation(){
   const user=await currentUser()
@@ -15,8 +15,8 @@ export async function GetUserSubscriptionInformation(){
   try {
     /*get subscriptions info from the bts database instead of directly from paystack*/
 
-    // const response1=await fetch(`${PUBLIC_BASE_URL}/api/subscriptions`,{next:{revalidate:60}})
-    const response1=await fetch(`${BTS_API_URL}/api/BydUsers/getAllUsers`,{method:'GET'})
+    const response1=await fetch(`${PUBLIC_BASE_URL}/api/subscriptions`,{next:{revalidate:60}})
+
     const allSubscriptionData=await response1.json()  
     const userEmailAddress = user?.emailAddresses[0].emailAddress.toLowerCase();
 
@@ -31,14 +31,23 @@ export async function GetUserSubscriptionInformation(){
             new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
         // )[0];
         ).slice(0,4);
-    // console.log("SUBINFO", userSubscriptionInformation);
+    console.log("SUBINFO", userSubscriptionInformation);
 
-    userSubscriptionInformation.forEach(async(subscription)=>{
+    // userSubscriptionInformation.forEach(async(subscription)=>{
+    //   if(subscription.amount!=600000 && subscription.status=="active" && userEmailAddress!=undefined){
+    //     await AddSubscriberToKit({email:userEmailAddress})
+    //   }
+    // })
+
+    /*uses promise.all over foreach loop to be more efficient*/
+    const kitUpdatePromises=userSubscriptionInformation.map(async(subscription)=>{
       if(subscription.amount!=600000 && subscription.status=="active" && userEmailAddress!=undefined){
-        // await AddSubscriberEmailToMailerLite({email:userEmailAddress})
-        await AddSubscriberToKit({email:userEmailAddress})
+        return AddSubscriberToKit({email:userEmailAddress})
       }
+      return null
     })
+    await Promise.all(kitUpdatePromises)
+
 
     return userSubscriptionInformation;
 
