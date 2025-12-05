6f3870a38227e60fa3a566d7c9ae353f0617abb7
Michael Gitonga <gitoshmbae@gmail.com>
2025-12-05 11:24:05 +0300

    Removes the byPassEmailAddresses array in src/app/Customer/page.tsx, src/app/Customer/find-jobs/page.tsx &  src/app/Customer/find-jobs/[jobsId]/page.tsx to be located in  src/staticData/Customer/

--- src/app/Customer/find-jobs/[jobsId]/page.tsx
+++ src/app/Customer/find-jobs/[jobsId]/page.tsx
@@ -2,6 +2,7 @@
 import { GetCustomerSubscriptionDetailsByCustomerIDFromPaystack } from "@/components/Customer/UserSubscriptionInformation";
 import ViewJob from "@/components/Customer/ViewJob";
 import SingleJobLoadingUI from "@/components/Loaders/SingleJobLoadingUI";
+import { byPassEmailAddresses } from "@/staticData/Customer/byPassSubscriptionCheck";
 import { subscriptionDetailsProps } from "@/types/subscriptions";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
@@ -13,74 +14,27 @@ export default async function page({
   params: Promise<{ jobsId: string }>;
 }) {
   const jobsId = (await params).jobsId;
+  
+  const user = await currentUser();
+  
 
   // const userSubscriptionInformation = await GetUserSubscriptionInformation();
   const userSubscriptionInformation:subscriptionDetailsProps[] = await GetCustomerSubscriptionDetailsByCustomerIDFromPaystack()
   
 
-  const jobsListingSubscriptionDetails = userSubscriptionInformation?.filter(
-    (subscription) =>
-      subscription.amount != 600000 &&
-      ["active", "attention", "non-renewing", "completed"].includes(
-        subscription.status.toLowerCase()
-      )
-  )[0];
-  const user = await currentUser();
+  // const jobsListingSubscriptionDetails = userSubscriptionInformation?.filter(
+  //   (subscription) =>
+  //     subscription.amount != 600000 &&
+  //     ["active", "attention", "non-renewing", "completed"].includes(
+  //       subscription.status.toLowerCase()
+  //     )
+  // )[0];
 
-  const byPassEmailAddresses = [
-    // `dennismugera254@gmail.com`,
-    // `victornmungai@gmail.com`,
-    // `wayuabritney@gmail.com`,
-    // `otienocynderella@gmail.com`,
-    // `mkabanesandra@gmail.com`,
-    // `marcellakemunto@yahoo.com`,
-    // `graceakinyi1991@gmail.com`,
-    // `nonigatura@gmail.com`,
-    // `fmmusembi96@gmail.com`,
-    // `henrychelele@gmail.com`,
-    // `a.wanjirunina@gmail.com`,
-    // `lilykimeu@gmail.com`,
-    // `kenmuvya22@gmail.com`,
-    // `mwawakamark@gmail.com`,
-    // `a.wanjirunina@gmail.com`,
-    // `cmweru2002@gmail.com`,
-    // `mutheustacey30@gmail.com`,
-    // `jsamande21@gmail.com`,
-    // `wanjirugichu7@gmail.com`,
-    // `bkirinya@gmail.com`,
-    // `k8karanja@gmail.com`,
-    // `mosesmwangi007@gmail.com`,
-    // `maureengakiavih@gmail.com`,
-    // `mercynderitu183@gmail.com`,
-    // `carolynmnjeri@gmail.com`,
-    // `hassenga54@gmail.com`,
-    // `belindaschira@gmail.com`,
-    // `wangui.c.njeri@gmail.com`,
-    // `jochesoli2015@gmail.com`,
-    // `willymathuva@gmail.com`,
-    // `winnie.gacheruw@gmail.com`,
-    // `sharleen.maina98@gmail.com`,
-    // `loismburuga@gmail.com`,
-    // `marthatemesghen@gmail.com`,
-    // `julietkaranja@gmail.com`,
-    // `kingoriwa@gmail.com`,
-    // `mdorcas864@gmail.com`,
-    // `daisygombe@gmail.com`,
-    // `sue.kinyanjui@gmail.com`,
-    // `megankesbai@gmail.com`,
-    // `faith.sikobe3@gmail.com`,
-    // `michellemasiga@gmail.com`,
-    // `nikolaiochwada@gmail.com`,
-    // `myrakagai05@gmail.com`,
-    // `zeldafaith@gmail.com`,
-    // `cwanjirumbugua@gmail.com`,
-    // `emanono@gmail.com`,
-    // `teddy254mutinge@gmail.com`,
-    // `myrakagai05@gmail.com`,
-    // `imokolabarbra@gmail.com`,
-    `patienceat63@gmail.com`,
-    `gitoshmbae@gmail.com`,
-  ];
+  const jobsListingSubscriptionDetails=userSubscriptionInformation.find((subscription)=>subscription.amount!=600000 &&
+["active", "attention", "non-renewing", "completed"].includes(subscription.status.toLowerCase()))
+
+
+  
 
   if (jobsListingSubscriptionDetails == undefined && !byPassEmailAddresses.includes(
       user?.emailAddresses[0].emailAddress as string
--- src/app/Customer/page.tsx
+++ src/app/Customer/page.tsx
@@ -6,6 +6,7 @@ import { GetCustomerSubscriptionDetailsByCustomerIDFromPaystack } from "@/compon
 // import { GetUserSubscriptionInformation } from "@/components/Customer/UserSubscriptionInformation";
 import DashboardPageLoader from "@/components/Loaders/DashboardPageLoader";
 import PackagesLoader from "@/components/Loaders/PackagesLoader";
+import { byPassEmailAddresses } from "@/staticData/Customer/byPassSubscriptionCheck";
 import { subscriptionDetailsProps } from "@/types/subscriptions";
 import { currentUser } from "@clerk/nextjs/server";
 import { Suspense } from "react";
@@ -20,60 +21,7 @@ export default async function CustomerDefaultPage() {
 
   // console.log("BTS USER FROM CUSTOMER PAGE",validUser)
 
-  const byPassEmailAddresses = [
-    // `dennismugera254@gmail.com`,
-    // `victornmungai@gmail.com`,
-    // `wayuabritney@gmail.com`,
-    // `otienocynderella@gmail.com`,
-    // `mkabanesandra@gmail.com`,
-    // `marcellakemunto@yahoo.com`,
-    // `graceakinyi1991@gmail.com`,
-    // `nonigatura@gmail.com`,
-    // `fmmusembi96@gmail.com`,
-    // `henrychelele@gmail.com`,
-    // `a.wanjirunina@gmail.com`,
-    // `lilykimeu@gmail.com`,
-    // `kenmuvya22@gmail.com`,
-    // `mwawakamark@gmail.com`,
-    // `a.wanjirunina@gmail.com`,
-    // `cmweru2002@gmail.com`,
-    // `mutheustacey30@gmail.com`,
-    // `jsamande21@gmail.com`,
-    // `wanjirugichu7@gmail.com`,
-    // `bkirinya@gmail.com`,
-    // `k8karanja@gmail.com`,
-    // `mosesmwangi007@gmail.com`,
-    // `maureengakiavih@gmail.com`,
-    // `mercynderitu183@gmail.com`,
-    // `carolynmnjeri@gmail.com`,
-    // `hassenga54@gmail.com`,
-    // `belindaschira@gmail.com`,
-    // `wangui.c.njeri@gmail.com`,
-    // `jochesoli2015@gmail.com`,
-    // `willymathuva@gmail.com`,
-    // `winnie.gacheruw@gmail.com`,
-    // `sharleen.maina98@gmail.com`,
-    // `loismburuga@gmail.com`,
-    // `marthatemesghen@gmail.com`,
-    // `julietkaranja@gmail.com`,
-    // `kingoriwa@gmail.com`,
-    // `mdorcas864@gmail.com`,
-    // `daisygombe@gmail.com`,
-    // `sue.kinyanjui@gmail.com`,
-    // `megankesbai@gmail.com`,
-    // `faith.sikobe3@gmail.com`,
-    // `michellemasiga@gmail.com`,
-    // `nikolaiochwada@gmail.com`,
-    // `myrakagai05@gmail.com`,
-    // `zeldafaith@gmail.com`,
-    // `cwanjirumbugua@gmail.com`,
-    // `emanono@gmail.com`,
-    // `teddy254mutinge@gmail.com`,
-    // `myrakagai05@gmail.com`,
-    //   `imokolabarbra@gmail.com`,
-    `patienceat63@gmail.com`,
-    `gitoshmbae@gmail.com`,
-  ];
+ 
 
   const allowByPassUser = byPassEmailAddresses.includes(
     user?.emailAddresses[0].emailAddress as string
@@ -90,7 +38,7 @@ export default async function CustomerDefaultPage() {
 
   return (
     <>
-      {/* {isValidSubscription ? <SubscriptionDetails /> : <PackageOptionSection />} */}
+      
       {isValidSubscription || allowByPassUser == true ? (
         <Suspense fallback={<DashboardPageLoader />}>
           <SubscriptionDetails />
