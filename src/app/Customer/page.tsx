4c76c778cec8ceadae4d79c8cca62374c37e670b
Michael Gitonga <gitoshmbae@gmail.com>
2026-01-05 11:13:50 +0300

    Adds parenthese for the checking of userSubscriptionInformation or allowBypassuser in src/app/Customer/page.tsx, removes imokolabarbra@gmail.com from the bypassEmailAddress array while removing commented out emails in src/staticData/Customer/byPassSubscriptionCheck.ts

--- src/app/Customer/page.tsx
+++ src/app/Customer/page.tsx
@@ -2,7 +2,10 @@
 
 import PackageOptionSection from "@/components/Customer/PackageOptionSection";
 import SubscriptionDetails from "@/components/Customer/SubscriptionDetails";
-import { AddNewSubscriberToDatabase, GetCustomerSubscriptionDetailsByCustomerIDFromPaystack } from "@/components/Customer/UserSubscriptionInformation";
+import {
+  AddNewSubscriberToDatabase,
+  GetCustomerSubscriptionDetailsByCustomerIDFromPaystack,
+} from "@/components/Customer/UserSubscriptionInformation";
 // import { GetUserSubscriptionInformation } from "@/components/Customer/UserSubscriptionInformation";
 import DashboardPageLoader from "@/components/Loaders/DashboardPageLoader";
 import PackagesLoader from "@/components/Loaders/PackagesLoader";
@@ -12,46 +15,50 @@ import { currentUser } from "@clerk/nextjs/server";
 import { Suspense } from "react";
 
 export default async function CustomerDefaultPage() {
-
-
-  
   // const userSubscriptionInformation = await GetUserSubscriptionInformation();
   // const userSubscriptionInformation:subscriptionDetailsProps[] = await GetCustomerSubscriptionDetailsByCustomerIDFromPaystack();
 
-  let userSubscriptionInformation:subscriptionDetailsProps[] | null= null;
+  let userSubscriptionInformation: subscriptionDetailsProps[] | null = null;
 
-   userSubscriptionInformation= await GetCustomerSubscriptionDetailsByCustomerIDFromPaystack()
+  userSubscriptionInformation =
+    await GetCustomerSubscriptionDetailsByCustomerIDFromPaystack();
 
-  
   const user = await currentUser();
-  
-  await AddNewSubscriberToDatabase()
+
+  await AddNewSubscriberToDatabase();
   // console.log("USER SUB INFO", userSubscriptionInformation)
 
   // console.log("BTS USER FROM CUSTOMER PAGE",validUser)
-
- 
 
   const allowByPassUser = byPassEmailAddresses.includes(
     user?.emailAddresses[0].emailAddress as string
   );
 
-  
-  const isValidSubscription = userSubscriptionInformation?.some(
-    (subscription) => {
-      return ["active", "attention", "non-renewing", "completed"].includes(
-        subscription.status.toLowerCase()
-      );
-    }
-  );
+  // const isValidSubscription = userSubscriptionInformation?.some(
+  //   (subscription) => {
+  //     return ["active", "attention", "non-renewing", "completed"].includes(
+  //       subscription.status.toLowerCase()
+  //     );
+  //   }
+  // );
   // const isValidSubscription=userSubscriptionInformation
 
   return (
     <>
-      
       {/* {isValidSubscription || allowByPassUser == true ? ( */}
       {/* {userSubscriptionInformation!=null || allowByPassUser == true ? ( */}
-      {isValidSubscription!=null || allowByPassUser == true ? (
+
+      {/* {isValidSubscription!=undefined || allowByPassUser == true ? (
+        <Suspense fallback={<DashboardPageLoader />}>
+          <SubscriptionDetails />
+        </Suspense>
+      ) : (
+        <Suspense fallback={<PackagesLoader />}>
+          <PackageOptionSection />
+        </Suspense>
+      )} */}
+
+      {(userSubscriptionInformation != null || allowByPassUser === true) ? (
         <Suspense fallback={<DashboardPageLoader />}>
           <SubscriptionDetails />
         </Suspense>
