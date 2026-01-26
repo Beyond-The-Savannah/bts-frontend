7af19615b7cea91525d490ccd7703984755bbee8
Michael Gitonga <gitoshmbae@gmail.com>
2026-01-26 18:02:13 +0300

    Adds metadata for the events page in src/app/(root)/events/page.tsx

--- src/app/(root)/events/page.tsx
+++ src/app/(root)/events/page.tsx
@@ -1,7 +1,28 @@
 import GuestSpeakes from "@/components/eventsPage/GuestSpeakes";
 import HeroSection from "@/components/eventsPage/HeroSection";
 import SupportSection from "@/components/eventsPage/SupportSection";
+import { Metadata } from "next";
+import { getCldImageUrl } from "next-cloudinary";
 
+
+const url = getCldImageUrl({
+  src: "events-page_q5pg9k",
+});
+
+export const metadata: Metadata = {
+  openGraph: {
+    images: [
+      {
+        width: 1200,
+        height: 627,
+        url,
+      },
+    ],
+  },
+  title: "Events - Beyond The Savannah",
+  description:
+    "The world is no longer confined to four walls, and neither is your career. Join Beyond the Savannah for an exclusive corporate mixer designed to bridge the gap between local expertise and global reach.",
+};
 
 export default function page() {
   return (
