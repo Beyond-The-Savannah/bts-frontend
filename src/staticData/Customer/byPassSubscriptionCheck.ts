739603acccbbb92b6627d64259a74ce66a8536b6
Michael Gitonga <gitoshmbae@gmail.com>
2026-02-09 11:57:13 +0300

    Removes two emails (patienceat63@gmail.com,veronica.gachoroba@gmail.com) from the byPassEmail addresses in src/staticData/Customer/byPassSubscriptionCheck.ts

--- src/staticData/Customer/byPassSubscriptionCheck.ts
+++ src/staticData/Customer/byPassSubscriptionCheck.ts
@@ -1,7 +1,7 @@
 export const byPassEmailAddresses = [
   // `imokolabarbra@gmail.com`,
-  `veronica.gachoroba@gmail.com`,
+  // `veronica.gachoroba@gmail.com`,
   `kiringakabaka@yahoo.com`,
-  `patienceat63@gmail.com`,
+  // `patienceat63@gmail.com`,
   `gitoshmbae@gmail.com`,
 ];
