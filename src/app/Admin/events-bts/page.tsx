0aa2af7dd7466e9ee93e6c431f0b3192233ce081
Michael Gitonga <gitoshmbae@gmail.com>
2026-03-04 15:31:16 +0300

    Adds the force dynamic and mb-10 in src/app/Admin/events-bts/page.tsx

--- src/app/Admin/events-bts/page.tsx
+++ src/app/Admin/events-bts/page.tsx
@@ -1,6 +1,7 @@
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { GetAllEvents } from "@/db/queries/eventsQuries";
 
+export const dynamic= "force-dynamic"
 
 export default async function page() {
     const attendees=await GetAllEvents()
@@ -12,7 +13,7 @@ export default async function page() {
             <h2 className="text-3xl font-semibold">BTS March Mixer Event</h2>
             <div className="border-2 rounded-md border-bts-GreenOne w-36 mb-8"></div>
         </div>
-        <div className="flex w-full items-center justify-end gap-4 mb-4">
+        <div className="flex w-full items-center justify-end gap-4 mb-10">
               <div className="border rounded-xl px-3 py-1">
            Attendees:{" "}
           <span className="font-bold">{attendees.length}/60</span>
@@ -40,7 +41,7 @@ export default async function page() {
                             <TableCell >{attendee.email}</TableCell>
                             <TableCell >{attendee.phoneNumber}</TableCell>
                             <TableCell className="text-[0.5rem] 2xl:text-[0.5rem]">{attendee.eventName}</TableCell>
-                            <TableCell className="text-[0.5rem] 2xl:text-xs">{new Date(attendee.createdAt).toLocaleDateString()} - {new Date(attendee.createdAt).toLocaleTimeString()}</TableCell>
+                            <TableCell className="text-[0.5rem] 2xl:text-xs">{new Date(attendee.createdAt).toDateString()} - {new Date(attendee.createdAt).toLocaleTimeString()}</TableCell>
                             {/* <TableCell className="text-[0.5rem] 2xl:text-xs">{new Date(attendee.updatedAt).toLocaleDateString()} - {new Date(attendee.updatedAt).toLocaleTimeString()}</TableCell> */}
                     </TableRow>
                         ))}
