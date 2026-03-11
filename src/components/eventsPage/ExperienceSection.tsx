50d59ca54e45530b9d24040f344188993c820158
Michael Gitonga <gitoshmbae@gmail.com>
2026-03-11 09:24:24 +0300

    Changes the less than or equaul to greater than or equal  check in both src/components/eventsPage/HeroSection.tsx & src/components/eventsPage/ExperienceSection.tsx

--- src/components/eventsPage/ExperienceSection.tsx
+++ src/components/eventsPage/ExperienceSection.tsx
@@ -13,49 +13,7 @@ export default function ExperienceSection({
   });
   return (
     <>
-      {/* <section className=" ">
-        <div className="container mx-auto min-h-[80dvh] grid py-20 gap-4  px-4">
-          <h2 className="text-center text-5xl font-bold">The Experience</h2>
-          <div className="flex flex-wrap md:flex-nowrap justify-center gap-12 my-10">
-            <div className="w-full md:w-4/12 border-2 rounded-xl px-4 py-8 space-y-8 bg-linear-to-tr from-bts-BrownTwo via-bts-GreenOne/20 to-bts-GreenOne/5">
-              <p className="text-center text-xl font-semibold">
-                Premier Global recruiters
-              </p>
-              <p className="text-center ">
-                This is your chance to understand what international firms are
-                searching for and how to position yourself for the global
-                market.
-              </p>
-            </div>
-            <div className="w-full md:w-4/12 border-2 rounded-xl px-4 py-8 space-y-8 bg-linear-to-tr from-bts-BrownTwo via-bts-GreenOne/20 to-bts-GreenOne/5">
-              <p className="text-center text-xl font-semibold">Global Insights</p>
-              <p className="text-center ">
-                Hear from two visionary keynote speakers who are leading the
-                charge in the remote work revolution. They will dive into the
-                future of digital nomadism, cross-border productivity, and how
-                to maintain a competitive edge from anywhere on the planet.
-              </p>
-            </div>
-            <div className="w-full md:w-4/12 border-2 rounded-xl px-4 py-8 space-y-8 bg-linear-to-tr from-bts-BrownTwo via-bts-GreenOne/20 to-bts-GreenOne/5">
-              <p className="text-center text-xl font-semibold">Curated Networking</p>
-              <p className="text-center ">
-                Connect with like-minded professionals, founders, and innovators
-                in a high-vibe atmosphere.
-              </p>
-            </div>
-          </div>
-          <div className="max-w-4xl mx-auto mt-10">
-            <div className="space-y-4 my-10">
-              <p className="text-center text-xl font-medium">Ready to take your office to the world?</p>
-              <p className="text-center">Space is limited to ensure quality networking opportunities. <br/>Don&apos;t miss your chance to meet the people who can help you land your next global role</p>
-              <p className="text-sm italic text-center">The venue has limited parking space, advised to use a cab.</p>
-            </div>
-              <div className="grid place-content-center">
-              {eventAttendees <= 60 ? <EventForm /> : <EventSoldOutButton />}
-              </div>
-          </div>
-        </div>
-      </section> */}
+    
       <section className="bg-[#f9f7f2]">
         {/* <div className="container mx-auto min-h-[80dvh] grid py-20 gap-4  px-4"> */}
         <div className="container mx-auto  py-20 gap-4  px-4">
@@ -108,7 +66,7 @@ export default function ExperienceSection({
               <p className="text-sm italic text-center">The venue has limited parking space, advised to use a cab.</p>
             </div>
               <div className="grid place-content-center">
-              {eventAttendees <= 52 ? <EventForm /> : <EventSoldOutButton />}
+              {eventAttendees >= 52 ? <EventForm /> : <EventSoldOutButton />}
               </div>
           </div>
         </div>
--- src/components/eventsPage/HeroSection.tsx
+++ src/components/eventsPage/HeroSection.tsx
@@ -15,22 +15,7 @@ export default function HeroSection({
 }) {
   return (
     <>
-      {/* <section className=" relative bg-linear-to-tr from-bts-GreenOne  via-bts-GreenOne/20 to-bts-GreenOne/5 ">
-        <div className=" container mx-auto min-h-[90dvh] flex flex-col justify-center  gap-12  px-4">
-          <h1 className="text-5xl md:text-7xl max-w-4xl z-1 text-slate-900 font-bold">
-            Remote & Ready:
-            <br /> A Remote Work Mixer
-          </h1>
-          {eventAttendees <= 60 ? <EventForm /> : <EventSoldOutButton />}
-        </div>
-        <DisplayImageFromNextCloudinary
-          src="march_event_mixer_event_hero_img_ar0yxs"
-          alt="Lorraine, Beyond The Savannah Founder"
-          height={800}
-          width={650}
-          classname=" absolute hidden lg:block bottom-0 right-1 z object-cover "
-        />
-      </section> */}
+    
       <section className="bg-linear-to-tr from-bts-GreenOne  via-bts-GreenOne/20 to-bts-GreenOne/5 ">
         <div className="container mx-auto px-4 xs:min-h-[99dvh] md:min-h-[90dvh] lg:min-h-[90dvh] xl:min-h-[90dvh] 2xl:min-h-[90dvh] grid grid-cols-1 md:grid-cols-12 gap-4">
           <div className=" cols-span-12  md:col-span-8 flex flex-col justify-center  gap-12  px-4">
@@ -38,7 +23,7 @@ export default function HeroSection({
               Remote & Ready:
               <br /> A Remote Work Mixer
             </h1>
-            {eventAttendees <= 52 ? <EventForm /> : <EventSoldOutButton />}
+            {eventAttendees >= 52 ? <EventForm /> : <EventSoldOutButton />}
           </div>
           <div className="cols-span-12  md:col-span-4">
             <DisplayImageFromNextCloudinary
