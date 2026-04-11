fb478fffd575fa8a2465eaf442502ae602aff74b
Michael Gitonga <gitoshmbae@gmail.com>
2026-04-11 17:27:59 +0300

    Adds husseinrahama1@gmail.com under the work remote bundle plus in src/staticData/Customer/byPassSubscriptionCheck.ts

--- src/staticData/Customer/byPassSubscriptionCheck.ts
+++ src/staticData/Customer/byPassSubscriptionCheck.ts
@@ -1,4 +1,7 @@
 export const byPassEmailAddresses = [
+//remote work bundle plus users
+`husseinrahama1@gmail.com`,
+
   `hannahmwangi866@gmail.com`,
   // `nancynyarige01@gmail.com`,
   // `kiringakabaka@yahoo.com`,
