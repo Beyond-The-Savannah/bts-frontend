c5715a1c831bc94674d8a78e1df3de4d088465d3
Michael Gitonga <gitoshmbae@gmail.com>
2026-04-23 15:04:24 +0300

    mend
    Adds a new email (linda.nyuguto@gmail.com) into the byPassEmailAddresses array in src/staticData/Customer/byPassSubscriptionCheck.ts

--- src/staticData/Customer/byPassSubscriptionCheck.ts
+++ src/staticData/Customer/byPassSubscriptionCheck.ts
@@ -2,6 +2,7 @@ export const byPassEmailAddresses = [
 //remote work bundle plus users
 `husseinrahama1@gmail.com`, //12-04-26
 `wachera.mugumo@gmail.com`, //15-04-26
+`linda.nyuguto@gmail.com`, //23-04-26
 
   `hannahmwangi866@gmail.com`,
   // `nancynyarige01@gmail.com`,
