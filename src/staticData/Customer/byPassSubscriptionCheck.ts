f1944e8c4dc980c084088871f46fe4daf0e00c74
Michael Gitonga <gitoshmbae@gmail.com>
2026-05-05 15:03:39 +0300

    Adds a new email (omabenenaftali@gmail.com) into the byPassEmailAddresses array in src/staticData/Customer/byPassSubscriptionCheck.ts

--- src/staticData/Customer/byPassSubscriptionCheck.ts
+++ src/staticData/Customer/byPassSubscriptionCheck.ts
@@ -3,6 +3,7 @@ export const byPassEmailAddresses = [
 `husseinrahama1@gmail.com`, //12-04-26
 `wachera.mugumo@gmail.com`, //15-04-26
 `linda.nyuguto@gmail.com`, //23-04-26
+`omabenenaftali@gmail.com`, //05-05-26
 
   `hannahmwangi866@gmail.com`,
   // `nancynyarige01@gmail.com`,
