52feafdce55de4c4e784114236c34ef255a4bc38
Michael Gitonga <gitoshmbae@gmail.com>
2026-05-22 09:02:30 +0300

    moves the <PoolOfCandidates/> component to be above <PartnershipTypes /> in src/app/(root)/partners/page.tsx

--- src/app/(root)/partners/page.tsx
+++ src/app/(root)/partners/page.tsx
@@ -32,8 +32,8 @@ export default function page() {
       <HeroSection />
       <WhyPartnerships />
       <FeaturedPartners />
-      <PartnershipTypes />
       <PoolOfCandidates/>
+      <PartnershipTypes />
       <PartnersCTASection />
     </>
   );
