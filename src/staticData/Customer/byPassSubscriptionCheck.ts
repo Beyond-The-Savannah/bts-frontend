b72bb46c8b0fc44e957054cd53af70319a3dc2c5
Michael Gitonga <gitoshmbae@gmail.com>
2026-06-15 13:48:57 +0300

    Adds richasante4321@gmail.com in src/staticData/Customer/byPassSubscriptionCheck.ts

--- src/staticData/Customer/byPassSubscriptionCheck.ts
+++ src/staticData/Customer/byPassSubscriptionCheck.ts
@@ -5,13 +5,14 @@ export const byPassEmailAddresses = [
 // `linda.nyuguto@gmail.com`, //23-04-26
 // `omabenenaftali@gmail.com`, //05-05-26
 // `lynmws@googlemail.com`, //05-05-26
-`ann33kimani@gmail.com`, //14-05-26
+// `ann33kimani@gmail.com`, //14-05-26
 `joanmuthuri@gmail.com`, //18-05-26
 `tkidula@gmail.com`, //21-05-26
 `victoria.wakiini@gmail.com`, //25-05-26 and 10-06-26
 `akoth2014sharon@gmail.com`, //12-06-26
 
-`aheebwasama@gmail.com`, //12-05-26
+`richasante4321@gmail.com`, //15-06-26
+// `aheebwasama@gmail.com`, //12-05-26
   // `hannahmwangi866@gmail.com`,
   // `nancynyarige01@gmail.com`,
   // `kiringakabaka@yahoo.com`,
